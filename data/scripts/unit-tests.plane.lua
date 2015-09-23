22cb2b75ea0abfec562eaf2b824529d896ddfd58
Aleksey Fedotov <0xc0dec@gmail.com>
2015-09-24 00:44:14 +0500

    Bind PlaneIntersection enum

--- data/scripts/unit-tests.plane.lua
+++ data/scripts/unit-tests.plane.lua
@@ -2,6 +2,9 @@ runTest(function()
 	local p = solo.Plane(solo.Vector3(1, 2, 3), 1)
 	local v = solo.Vector3(1, 2, 3)
 	solo.Plane.intersection(p, p, p, v)
+	local i = solo.Plane.Intersection_Front
+	i = solo.Plane.Intersection_Back
+	i = solo.Plane.Intersection_Intersecting
 	p:getNormal()
 	p:setNormal(solo.Vector3(1, 2, 3))
 	p:getDistance()
