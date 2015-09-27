a478eb31ee0b94e9445188c011bd4b892b6ce9bb
Aleksey Fedotov <0xc0dec@gmail.com>
2015-09-27 23:40:23 +0500

    Fixed depth testing: proper EngineCreationArgs initialization

--- include/SoloEngineCreationArgs.h
+++ include/SoloEngineCreationArgs.h
@@ -27,10 +27,10 @@ namespace solo
 			bool fullScreen = false,
 			const std::string& windowTitle = ""):
 			mode(mode),
-			canvasWidth(canvasWidth),
-			canvasHeight(canvasHeight),
-			bits(bits),
-			depth(depth),
+			canvasWidth(canvasWidth > 0 ? canvasWidth : 1),
+			canvasHeight(canvasHeight > 0 ? canvasHeight : 1),
+			bits(bits > 0 ? bits : 32),
+			depth(depth > 0 ? depth : 16),
 			fullScreen(fullScreen),
 			windowTitle(windowTitle)
 		{
