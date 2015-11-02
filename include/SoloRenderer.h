bbe81aa9bc246024832b124e4d5a0578a64adb26
Aleksey Fedotov <0xc0dec@gmail.com>
2015-11-03 00:14:22 +0500

    Removed unused methods

--- include/SoloRenderer.h
+++ include/SoloRenderer.h
@@ -10,21 +10,5 @@ namespace solo
 		virtual ~Renderer() {}
 
 		virtual void render(RenderContext& context) = 0;
-
-		unsigned getRenderQueue() const;
-		void setRenderQueue(unsigned queue);
-
-	private:
-		unsigned renderQueue = 0;
 	};
-
-	inline unsigned Renderer::getRenderQueue() const
-	{
-		return renderQueue;
-	}
-
-	inline void Renderer::setRenderQueue(unsigned queue)
-	{
-		renderQueue = queue;
-	}
 }
