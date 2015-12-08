6c511bdc33c38b628e122a5ed8641759f1d46161
Aleksey Fedotov <0xc0dec@gmail.com>
2015-12-09 00:38:48 +0500

    Rewrote obj loader - removed stringstream usages to speed up

--- src/SoloObjModelLoader.cpp
+++ src/SoloObjModelLoader.cpp
@@ -26,7 +26,7 @@ Vector3 parseVector3(const char *from, const char *to)
 	float result[3];
 	size_t bufIdx = 0;
 	size_t resultIdx = -1;
-	for (; from <= to; from += 1)
+	for (; from <= to; ++from)
 	{
 		auto c = *from;
 		if ((from == to || isspace(c)) && bufIdx > 0)
@@ -41,6 +41,35 @@ Vector3 parseVector3(const char *from, const char *to)
 			buf[bufIdx++] = c;
 	}
 	return Vector3(result[0], result[1], result[2]);
+}
+
+
+void parseIndexes(const char **from, const char *to, unsigned **result)
+{
+	char buf[16];
+	size_t bufIdx = 0;
+	size_t resultIdx = 0;
+	for (; *from <= to; *from += 1)
+	{
+		auto c = **from;
+		auto okSym = (c >= '0' && c <= '9') || c == '-' || c == '.';
+		if (okSym)
+			buf[bufIdx++] = c;
+		
+		if (!okSym || *from == to)
+		{
+			if (bufIdx > 0)
+			{
+				buf[bufIdx] = '\0';
+				*result[resultIdx++] = atof(buf);
+				bufIdx = 0;
+			}
+			else if (resultIdx > 0)
+				result[resultIdx++] = nullptr;
+			if (resultIdx > 2)
+				break;
+		}
+	}
 }
 
 
@@ -78,42 +107,47 @@ shared<Model> ObjModelLoader::load(const std::string& uri)
 	};
 	
 	std::string meshName;
-	std::string threes[3];
 	fs->iterateLines(uri, [&](const std::string& line)
 	{
-		std::istringstream l(line);
-		std::string type;
 		auto lineSize = line.size();
-		l >> type;
-		if (type == "v")
-			inputVertices.push_back(parseVector3(line.c_str() + 2, line.c_str() + lineSize - 2));
-		else if (type == "vn")
-			inputNormals.push_back(parseVector3(line.c_str() + 3, line.c_str() + lineSize - 3));
-		else if (type == "vt")
+		if (line[0] == 'v')
 		{
-			auto uv = parseVector3(line.c_str() + 3, line.c_str() + lineSize - 3);
-			inputUvs.push_back(Vector2(uv.x, uv.y));
-		}
-		else if (type == "f")
-		{
-			char dummy;
-			auto from = line.c_str() + 2;
-			l >> threes[0] >> threes[1] >> threes[2];
-			for (const auto &three: threes)
+			if (line[1] == 'n')
+				inputNormals.push_back(parseVector3(line.c_str() + 3, line.c_str() + lineSize - 3));
+			else if (line[1] == 't')
 			{
+				auto uv = parseVector3(line.c_str() + 3, line.c_str() + lineSize - 3);
+				inputUvs.push_back(Vector2(uv.x, uv.y));
+			}
+			else
+				inputVertices.push_back(parseVector3(line.c_str() + 2, line.c_str() + lineSize - 2));
+		}
+		else if (line[0] == 'f')
+		{
+			auto from = line.c_str() + 2;
+			auto to = from + lineSize - 3;
+			unsigned spaceIdx = 1;
+			unsigned vIdx, uvIdx, nIdx;
+			unsigned *idxs[] = { &vIdx, &uvIdx, &nIdx };
+			std::string three;
+			for (auto i = 0; i < 3; ++i)
+			{
+				auto nextSpaceIdx = line.find(' ', spaceIdx + 1);
+				three.assign(line.substr(spaceIdx + 1, nextSpaceIdx != std::string::npos ? (nextSpaceIdx - spaceIdx - 1): lineSize - spaceIdx));
+				spaceIdx = nextSpaceIdx;
 				auto it = uniqueIndices.find(three);
 				if (it != uniqueIndices.end())
+				{
 					indices.push_back(it->second);
+					from += three.size() + 1;
+				}
 				else
 				{
-					std::istringstream ss(three);
-					unsigned vIdx, uvIdx, nIdx;
-					ss >> vIdx >> dummy;
+					parseIndexes(&from, to, idxs);
 					vertices.push_back(inputVertices[vIdx - 1]);
-					if (ss >> uvIdx)
+					if (idxs[1])
 						uvs.push_back(inputUvs[uvIdx - 1]);
-					ss >> dummy;
-					if (ss >> nIdx)
+					if (idxs[2])
 						normals.push_back(inputNormals[nIdx - 1]);
 					auto newIndex = static_cast<uint16_t>(vertices.size() - 1);
 					uniqueIndices[three] = newIndex;
@@ -121,11 +155,11 @@ shared<Model> ObjModelLoader::load(const std::string& uri)
 				}
 			}
 		}
-		else if (type == "o")
+		else if (line[0] == 'o')
 		{
 			if (!vertices.empty())
 				finishObject(meshName);
-			l >> meshName;
+			meshName.assign(line.substr(2));
 		}
 		return true;
 	});
