fcbda1e20efe38fc1330425d17c7539b278cffdd
Aleksey Fedotov <0xc0dec@gmail.com>
2016-01-07 11:25:56 +0500

    Moved Mesh2 into the same files as Mesh

--- src/SoloMesh.cpp
+++ src/SoloMesh.cpp
@@ -5,6 +5,22 @@
 using namespace solo;
 
 
+shared<Mesh2> Mesh2::create(DeviceMode mode, const VertexFormat &vertexFormat, bool dynamic)
+{
+	//	if (mode == DeviceMode::OpenGL)
+	//		return
+	// TODO stub mesh2
+	return SL_NEW_SHARED(OpenGLMesh2, vertexFormat, dynamic);
+}
+
+
+Mesh2::Mesh2(const VertexFormat& vertexFormat, bool dynamic) :
+	vertexFormat(vertexFormat),
+	dynamic(dynamic)
+{
+}
+
+
 shared<Mesh> Mesh::create(DeviceMode mode)
 {
     if (mode == DeviceMode::OpenGL)
--- src/platform/opengl/SoloOpenGLMesh.h
+++ src/platform/opengl/SoloOpenGLMesh.h
@@ -3,10 +3,25 @@
 #include "SoloBase.h"
 #include "SoloMesh.h"
 #include "SoloVector3.h"
+#include "SoloVertexFormat.h"
 #include <GL/glew.h>
 
 namespace solo
 {
+	class OpenGLMesh2 : public Mesh2
+	{
+	public:
+		virtual void setVertexData(float *data, unsigned vertexCount, unsigned firstVertexIndex) override;
+
+	private:
+		friend class Mesh2;
+
+		OpenGLMesh2(const VertexFormat &vertexFormat, bool dynamic);
+
+		GLuint vertexBufferHandle = 0;
+		unsigned lastVertexCount = 0;
+	};
+
     class OpenGLMesh : public Mesh
     {
     public:
