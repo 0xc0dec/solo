28fc0b5325abf725cd7f0dfd02011526e879d970
Aleksey Fedotov <0xc0dec@gmail.com>
2016-01-10 18:15:25 +0500

    Noncopyable ScriptManager base

--- include/SoloScriptManager.h
+++ include/SoloScriptManager.h
@@ -7,11 +7,15 @@ namespace solo
     class ScriptManager
     {
     public:
-        virtual ~ScriptManager() {}
-
         static shared<ScriptManager> create();
+
+        SL_NONCOPYABLE(ScriptManager);
+        virtual ~ScriptManager() {}
 
         virtual void execute(const std::string &code) = 0;
         virtual void executeFile(const std::string &path) = 0;
+
+    protected:
+        ScriptManager() {}
     };
 }
