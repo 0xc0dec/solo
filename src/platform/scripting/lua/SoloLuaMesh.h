9f06f8e57f2c9795614e5ce61b9d091dcb04a60e
Aleksey Fedotov <0xc0dec@gmail.com>
2016-02-01 00:24:14 +0500

    Removed TODO

--- src/platform/scripting/lua/SoloLuaMesh.h
+++ src/platform/scripting/lua/SoloLuaMesh.h
@@ -10,7 +10,6 @@ namespace solo
     {
     public:
         static int addIndex(Mesh* mesh);
-        // TODO other index methods
         static void resetData(Mesh* mesh, int slot, const std::vector<float>& data, int elementCount, bool dynamic);
         static void updateData(Mesh* mesh, int slot, const std::vector<float>& data, int elementCount, int updateFromIndex);
         static void resetIndexData(Mesh* mesh, int index, const std::vector<uint16_t>& data, int elementCount, bool dynamic);
