6899162475d15c84a569f6d7642468adf12678fe
Aleksey Fedotov <0xc0dec@gmail.com>
2016-02-24 23:43:18 +0500

    More exception types

--- include/SoloException.h
+++ include/SoloException.h
@@ -7,6 +7,15 @@
 #define SL_THROW(TExc, ...) throw TExc(__VA_ARGS__)
 #define SL_THROW_FMT(TExc, ...) throw TExc(SL_FMT(__VA_ARGS__))
 
+#define SL_DEFINE_SIMPLE_EXCEPTION(type) \
+    class type: public EngineException \
+    { \
+    public: \
+        explicit type(const std::string& msg) : EngineException(msg) \
+        { \
+        } \
+    };
+
 namespace solo
 {
     class EngineException: public std::runtime_error
@@ -17,11 +26,19 @@ namespace solo
         }
     };
 
-    class EffectCompilationException: public EngineException
+    SL_DEFINE_SIMPLE_EXCEPTION(InternalException);
+    SL_DEFINE_SIMPLE_EXCEPTION(InvalidOperationException);
+    SL_DEFINE_SIMPLE_EXCEPTION(InvalidInputException);
+    SL_DEFINE_SIMPLE_EXCEPTION(IOException);
+    SL_DEFINE_SIMPLE_EXCEPTION(ResourceException);
+
+    class EffectCompilationException: public ResourceException
     {
     public:
         std::string log;
 
-        EffectCompilationException(const std::string& message, const std::string& log) : EngineException(message), log(log) {}
+        EffectCompilationException(const std::string& message, const std::string& log) : ResourceException(message), log(log)
+        {
+        }
     };
 }
