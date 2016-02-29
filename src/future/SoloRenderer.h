9159c658022cbe20a90a32ed350f3abdb5c016c3
Aleksey Fedotov <0xc0dec@gmail.com>
2016-03-01 00:34:29 +0500

    Thinking out the low level renderer interface

--- src/future/SoloRenderer.h
+++ src/future/SoloRenderer.h
@@ -5,6 +5,72 @@ namespace solo
     class Renderer
     {
     public:
+        void createVertexBuffer() {}
+        void destroyVertexBuffer() {}
 
+        void createDynamicIndexBuffer() {}
+        void updateDynamicIndexBuffer() {}
+        void destroyDynamicIndexBuffer() {}
+
+        void createDynamicVertexBuffer() {}
+        void updateDynamicVertexBuffer() {}
+        void destroyDynamicVertexBuffer() {}
+
+        void createShader() {}
+        void getShaderUniforms() {}
+        void destroyShader() {}
+
+        void createProgram() {}
+        void destroyProgram() {}
+
+        void create2DTexture() {}
+        void update2DTexture() {}
+
+        void create3DTexture() {} 
+        void update3DTexture() {}
+
+        void createCubeTexture() {}
+        void updateCubeTexture() {}
+
+        void destroyTexture() {}
+
+        void createFrameBuffer() {}
+        void destroyFrameBuffer() {}
+
+        void createUniform() {}
+        void destroyUniform() {}
+
+        void createOcclusionQuery() {}
+        void getOcclusionQueryResult() {}
+        void destroyOcclusionQuery() {}
+
+        void setViewRect() {}
+        void setViewClear() {}
+
+        void setViewFrameBuffer() {}
+
+        void setViewTransform() {}
+
+        void resetView() {}
+
+        void setState() {}
+
+        void setOcclusionCondition() {}
+
+        void setStencil() {}
+
+        void setTransform() {}
+
+        void setUniform() {}
+
+        void setIndexBuffer() {}
+
+        void setVertexBuffer() {}
+
+        void setTexture() {}
+
+        void submitForRendering() {}
+
+        void blit() {}
     };
 }
