e18ad1ff5deb3ce19859bc9f23e6f0026172007a
Aleksey Fedotov <0xc0dec@gmail.com>
2016-03-03 01:15:52 +0500

    Cleanup

--- include/SoloBase.h
+++ include/SoloBase.h
@@ -11,19 +11,19 @@
 #include "SoloFormatter.h"
 #include "SoloException.h"
 
+#define SL_SHARED_STATIC_CAST		std::static_pointer_cast
+#define SL_MAKE_SHARED				std::make_shared
+#define SL_NEW_SHARED(type, ...) 	std::shared_ptr<type>(new type(__VA_ARGS__))
+
+#define SL_NONCOPYABLE(type) \
+    type(const type &other) = delete; \
+    type(type &&other) = delete; \
+    type &operator=(const type &other) = delete; \
+    type &operator=(type &&other) = delete;
+
 namespace solo
 {
-#	define SL_SHARED_STATIC_CAST		std::static_pointer_cast
-#	define SL_MAKE_SHARED				std::make_shared
-#	define SL_NEW_SHARED(type, ...) 	std::shared_ptr<type>(new type(__VA_ARGS__))
-
-#   define SL_NONCOPYABLE(type) \
-        type(const type &other) = delete; \
-        type(type &&other) = delete; \
-        type &operator=(const type &other) = delete; \
-        type &operator=(type &&other) = delete;
-
-    // Having this pointer means that the code owns the pointed object (just like, maybe, several other places).
+    // The presense of this pointer indicates that the surrounding code owns the pointed object OR shares ownership with others.
     // Code that works with raw pointers doesn't claim to own the pointed object.
     template <typename T> using shared = std::shared_ptr<T>;
 }
