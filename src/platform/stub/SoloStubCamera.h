f47073f58bbdb61a3c92f73c9a8e6d3b54ca32c8
Aleksey Fedotov <0xc0dec@gmail.com>
2016-03-10 01:17:57 +0500

    Commented out stub camera

--- src/platform/stub/SoloStubCamera.h
+++ src/platform/stub/SoloStubCamera.h
@@ -1,22 +1,22 @@
-#pragma once
-
-#include "SoloCamera.h"
-
-namespace solo
-{
-    class StubCamera: public Camera
-    {
-    protected:
-        virtual void applyViewport() override {}
-        virtual void applyClearColor() override {}
-        virtual void clear() override {}
-
-    private:
-        friend class Camera;
-
-        StubCamera(Scene* scene, const Node& node):
-            Camera(scene, node)
-        {
-        }
-    };
-}
+//#pragma once
+//
+//#include "SoloCamera.h"
+//
+//namespace solo
+//{
+//    class StubCamera: public Camera
+//    {
+//    protected:
+//        virtual void applyViewport() override {}
+//        virtual void applyClearColor() override {}
+//        virtual void clear() override {}
+//
+//    private:
+//        friend class Camera;
+//
+//        StubCamera(Scene* scene, const Node& node):
+//            Camera(scene, node)
+//        {
+//        }
+//    };
+//}
--- src/platform/stub/SoloStubEffectVariable.h
+++ src/platform/stub/SoloStubEffectVariable.h
@@ -1,33 +1,33 @@
-#pragma once
-
-#include "SoloEffectVariable.h"
-
-namespace solo
-{
-    class StubEffectVariable: public EffectVariable
-    {
-    public:
-        virtual void setFloat(float value) override {}
-        virtual void setFloatArray(const std::vector<float>& value) override {}
-        virtual void setInt(int value) override {}
-        virtual void setIntArray(const std::vector<int>& value) override {}
-        virtual void setVector2(const Vector2& value) override {}
-        virtual void setVector2Array(const std::vector<Vector2>& value) override {}
-        virtual void setVector3(const Vector3& value) override {}
-        virtual void setVector3Array(const std::vector<Vector3>& value) override {}
-        virtual void setVector4(const Vector4& value) override {}
-        virtual void setVector4Array(const std::vector<Vector4>& value) override {}
-        virtual void setMatrix(const Matrix& value) override {}
-        virtual void setMatrixArray(const std::vector<Matrix>& value) override {}
-        virtual void setTexture(const shared<Texture> texture) override {}
-        virtual void setTextureArray(const std::vector<shared<Texture>>& textures) override {}
-
-    private:
-        friend class StubEffect;
-
-        explicit StubEffectVariable(const std::string& name)
-            : EffectVariable(name)
-        {
-        }
-    };
-}
+//#pragma once
+//
+//#include "SoloEffectVariable.h"
+//
+//namespace solo
+//{
+//    class StubEffectVariable: public EffectVariable
+//    {
+//    public:
+//        virtual void setFloat(float value) override {}
+//        virtual void setFloatArray(const std::vector<float>& value) override {}
+//        virtual void setInt(int value) override {}
+//        virtual void setIntArray(const std::vector<int>& value) override {}
+//        virtual void setVector2(const Vector2& value) override {}
+//        virtual void setVector2Array(const std::vector<Vector2>& value) override {}
+//        virtual void setVector3(const Vector3& value) override {}
+//        virtual void setVector3Array(const std::vector<Vector3>& value) override {}
+//        virtual void setVector4(const Vector4& value) override {}
+//        virtual void setVector4Array(const std::vector<Vector4>& value) override {}
+//        virtual void setMatrix(const Matrix& value) override {}
+//        virtual void setMatrixArray(const std::vector<Matrix>& value) override {}
+//        virtual void setTexture(const shared<Texture> texture) override {}
+//        virtual void setTextureArray(const std::vector<shared<Texture>>& textures) override {}
+//
+//    private:
+//        friend class StubEffect;
+//
+//        explicit StubEffectVariable(const std::string& name)
+//            : EffectVariable(name)
+//        {
+//        }
+//    };
+//}
