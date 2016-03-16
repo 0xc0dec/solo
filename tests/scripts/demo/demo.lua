c8c06e8663537dd0340ac65b196329c3c0e71cb9
Aleksey Fedotov <0xc0dec@gmail.com>
2016-03-16 11:12:41 +0500

    Fixed demo selection

--- tests/scripts/demo/demo.lua
+++ tests/scripts/demo/demo.lua
@@ -7,39 +7,36 @@ function runDemo(path)
 	args.logFilePath = "demo.log"
 	local device = solo.Device.create(args)
 	local demo = dofile(path)
-	demo:init(device)
-	-- local _, err = pcall(function()
-	-- 	demo:init(device)
-	-- end)
-	-- if err then
-	-- 	device:getLogger():logCritical(err)
-	-- end
-	--
+
+	local _, err = pcall(function()
+		demo:init(device)
+	end)
+	if err then
+		device:getLogger():logCritical(err)
+	end
+
 	device:run()
+	device:reset()
 
 	demo = nil
 	device = nil
-
 	collectgarbage()
 end
 
 function prompt()
-	io.write("Available demos:\n")
+	io.write("\nAvailable demos:\n")
 	for i, info in ipairs(demos) do
 		print(i .. ": " .. info[1])
 	end
 
-	io.write("Which demo to run?: ")
+	io.write("Demo to run: ")
 	local number = tonumber(io.read())
 	local demoInfo = demos[number]
 	if demoInfo then
 		runDemo(demoInfo[2])
-		collectgarbage()
 		return true
-	else
-		io.write("No such demo")
-		return false
 	end
+	return false
 end
 
 while prompt() do
