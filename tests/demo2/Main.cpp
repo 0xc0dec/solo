7074af4d9e2b7bb037fee3b97c7b6f592a1937de
Aleksey Fedotov <0xc0dec@gmail.com>
2016-04-18 22:26:01 +0500

    Moved post processor from demo3 to demo2 and made effects switchable

--- tests/demo2/Main.cpp
+++ tests/demo2/Main.cpp
@@ -8,7 +8,7 @@
 using namespace solo;
 
 
-class PostProcessor: public ComponentBase<PostProcessor>
+class PostProcessor final: public ComponentBase<PostProcessor>
 {
 public:
     explicit PostProcessor(const Node& node):
@@ -22,6 +22,7 @@ public:
     virtual void init() override final
     {
         auto canvasSize = device->getCanvasSize();
+
         auto camera = node.getComponent<Camera>();
         fbTex = Texture2D::create();
         fbTex->setData(ColorFormat::RGB, {}, static_cast<uint32_t>(canvasSize.x), static_cast<uint32_t>(canvasSize.y));
@@ -39,8 +40,8 @@ public:
         fb2->setAttachments({ fbTex2 });
 
         auto grayscaleEffect = Effect::create(commonShaders.vertex.passThrough, fsGrayscale);
-        material = Material::create(grayscaleEffect);
-        material->setFloatParameter("rightSeparator", 0.25f);
+        grayscaleMat = Material::create(grayscaleEffect);
+        grayscaleMat->setFloatParameter("rightSeparator", 0.25f);
 
         auto saturateEffect = Effect::create(commonShaders.vertex.passThrough, fsSaturate);
         saturateMat = Material::create(saturateEffect);
@@ -58,11 +59,16 @@ public:
         horizontalBlurMat->setFloatParameter("rightSeparator", 0.75f);
     }
 
+    virtual void terminate() override final
+    {
+        node.getComponent<Camera>()->setRenderTarget(nullptr);
+    }
+
     virtual void onAfterCameraRender() override final
     {
         // bounce between the two frame buffers
-        material->setTextureParameter("mainTex", fbTex);
-        graphics->blit(material.get(), fb2.get());
+        grayscaleMat->setTextureParameter("mainTex", fbTex);
+        graphics->blit(grayscaleMat.get(), fb2.get());
 
         saturateMat->setTextureParameter("mainTex", fbTex2);
         graphics->blit(saturateMat.get(), fb1.get());
@@ -82,10 +88,118 @@ private:
     sptr<FrameBuffer> fb2 = nullptr;
     sptr<Texture2D> fbTex = nullptr;
     sptr<Texture2D> fbTex2 = nullptr;
-    sptr<Material> material = nullptr;
+    sptr<Material> grayscaleMat = nullptr;
     sptr<Material> saturateMat = nullptr;
     sptr<Material> verticalBlurMat = nullptr;
     sptr<Material> horizontalBlurMat = nullptr;
+};
+
+
+class PostProcessor2 final: public ComponentBase<PostProcessor2>
+{
+public:
+    explicit PostProcessor2(const Node& node) :
+        ComponentBase<PostProcessor2>(node),
+        device(Device::get()),
+        loader(Device::get()->getAssetLoader()),
+        graphics(Device::get()->getGraphics())
+    {
+    }
+
+    virtual void init() override final
+    {
+        const float stitchWidth = 30;
+
+        stitchTex = loader->loadTexture2D("../assets/stitches.png");
+        stitchTex->setFiltering(TextureFiltering::Nearest);
+
+        auto canvasSize = device->getCanvasSize();
+        auto stitchTexSize = stitchTex->getSize();
+
+        auto resolution = Vector2(
+            Math::clamp(static_cast<int>(canvasSize.x / stitchWidth) * 2, 1, 2048),
+            Math::clamp(static_cast<int>(canvasSize.y / stitchTexSize.y) * 2, 1, 2048));
+
+        auto stitchCount = Vector2(
+            resolution.x * stitchWidth / (2 * stitchTexSize.x),
+            resolution.y / 2);
+
+        auto camera = node.getComponent<Camera>();
+        fbTex = Texture2D::create();
+        fbTex->setData(ColorFormat::RGB, {}, static_cast<uint32_t>(resolution.x), static_cast<uint32_t>(resolution.y));
+        fbTex->setFiltering(TextureFiltering::Nearest);
+        fbTex->setWrapping(TextureWrapping::Clamp);
+        fb1 = FrameBuffer::create();
+        fb1->setAttachments({ fbTex });
+        camera->setViewport(0, 0, resolution.x, resolution.y);
+        camera->setRenderTarget(fb1);
+
+        auto effect = Effect::create(commonShaders.vertex.passThrough, fsStitches);
+        material = Material::create(effect);
+        material->setTextureParameter("mainTex", fbTex);
+        material->setTextureParameter("stitchTex", stitchTex);
+        material->setVector2Parameter("stitchCount", stitchCount);
+        material->setVector2Parameter("resolution", resolution);
+    }
+
+    virtual void terminate() override final
+    {
+        auto canvasSize = device->getCanvasSize();
+        auto camera = node.getComponent<Camera>();
+        camera->setRenderTarget(nullptr);
+        camera->setViewport(0, 0, canvasSize.x, canvasSize.y);
+    }
+
+    virtual void onAfterCameraRender() override final
+    {
+        graphics->blit(material.get(), nullptr);
+    }
+
+private:
+    Device* device;
+    AssetLoader* loader;
+    Graphics* graphics;
+    sptr<Texture2D> stitchTex;
+    sptr<FrameBuffer> fb1;
+    sptr<Texture2D> fbTex;
+    sptr<Material> material;
+};
+
+
+class ModeSwitcher final: public ComponentBase<ModeSwitcher>
+{
+public:
+    explicit ModeSwitcher(const Node& node, Device* device):
+        ComponentBase<ModeSwitcher>(node),
+        device(device)
+    {
+    }
+
+    virtual void update() override final
+    {
+        if (device->isKeyPressed(KeyCode::Digit1, true))
+        {
+            if (node.findComponent<PostProcessor>())
+                return;
+            node.removeComponent<PostProcessor2>();
+            node.addComponent<PostProcessor>();
+        }
+        if (device->isKeyPressed(KeyCode::Digit2, true))
+        {
+            if (node.findComponent<PostProcessor2>())
+                return;
+            node.removeComponent<PostProcessor>();
+            node.addComponent<PostProcessor2>();
+        }
+        if (device->isKeyPressed(KeyCode::Digit3, true))
+        {
+            node.removeComponent<PostProcessor>();
+            node.removeComponent<PostProcessor2>();
+        }
+    }
+
+private:
+    Device* device;
 };
 
 
@@ -98,6 +212,7 @@ public:
         initCamera();
         initSkybox();
         initMesh();
+        device->getLogger()->logInfo("Press keys 1..3 to switch between modes");
         device->run();
     }
 
@@ -124,7 +239,8 @@ public:
         cam->setNear(0.05f);
         node->addComponent<Spectator>();
         node->addComponent<EscapeWatcher>();
-        node->addComponent<PostProcessor>();
+        node->addComponent<PostProcessor2>();
+        node->addComponent<ModeSwitcher>(device);
         node->addComponent<Screenshoter>("demo2-screenshot.bmp");
     }
 
