14ff43a00228f294b14f662827200c2900db9123
Aleksey Fedotov <0xc0dec@gmail.com>
2016-04-20 00:50:36 +0500

    Removed SoloBullet*.h/cpp files - not really needed, moved Bullet impls into the corresponding .cpp files

--- src/SoloRigidBody.cpp
+++ src/SoloRigidBody.cpp
@@ -1,7 +1,33 @@
 #include "SoloRigidBody.h"
-#include "SoloBulletRigidBody.h"
+#include <btBulletDynamicsCommon.h>
 
 using namespace solo;
+
+
+class BulletRigidBody final : public RigidBody
+{
+public:
+
+private:
+    friend class RigidBody;
+
+    BulletRigidBody(Node node, const RigidBodyConstructionParameters& parameters);
+
+    uptr<btRigidBody> body;
+};
+
+
+BulletRigidBody::BulletRigidBody(Node node, const RigidBodyConstructionParameters& parameters) :
+    RigidBody(node)
+{
+    btRigidBody::btRigidBodyConstructionInfo info(parameters.mass, nullptr, nullptr/*, TODO */);
+    info.m_friction = parameters.friction;
+    info.m_restitution = parameters.restitution;
+    info.m_linearDamping = parameters.linearDamping;
+    info.m_angularDamping = parameters.angularDamping;
+
+    body = std::make_unique<btRigidBody>(info);
+}
 
 
 uptr<RigidBody> RigidBody::create(Node node, const RigidBodyConstructionParameters& parameters)
