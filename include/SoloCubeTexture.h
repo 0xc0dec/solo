bb3081e20379c9798a747b90d931dec1c056238a
Aleksey Fedotov <0xc0dec@gmail.com>
2016-05-14 02:00:38 +0500

    Formatting

--- include/SoloCubeTexture.h
+++ include/SoloCubeTexture.h
@@ -14,13 +14,12 @@ namespace solo
 
         virtual void bind() override final;
         virtual void generateMipmaps() override final;
-
-        void setData(CubeTextureFace face, TextureFormat format, const uint8_t* data, uint32_t width, uint32_t height);
-        
         virtual void setWrapping(TextureWrapping wrapping) override final;
 
         auto getDepthWrapping() const -> TextureWrapping;
         void setDepthWrapping(TextureWrapping depthWrap);
+
+        void setData(CubeTextureFace face, TextureFormat format, const uint8_t* data, uint32_t width, uint32_t height);
 
     protected:
         CubeTexture();
