f4174aaf76ce7e4ac37ebb152956c8ddf3486a00
Aleksey Fedotov <0xc0dec@gmail.com>
2016-05-21 13:11:45 +0500

    Moved TextureFlags to struct

--- include/SoloRenderer.h
+++ include/SoloRenderer.h
@@ -33,26 +33,26 @@ namespace solo
     SL_RENDERER_RESOURCE_HANDLE(VertexObjectHandle)
     SL_RENDERER_RESOURCE_HANDLE(UniformHandle)
 
-    enum class TextureFlags
+    struct TextureFlags
     {
-        MinFilterNearest = 1 << 0,
-        MinFilterLinear = 1 << 1,
-        MinFilterNearestMipmapNearest = 1 << 2,
-        MinFilterLinearMipmapNearest = 1 << 3,
-        MinFilterNearestMipmapLinear = 1 << 4,
-        MinFilterLinearMipmapLinear = 1 << 5,
-        MagFilterNearest = 1 << 6,
-        MagFilterLinear = 1 << 7,
-        MagFilterNearestMipmapNearest = 1 << 8,
-        MagFilterLinearMipmapNearest = 1 << 9,
-        MagFilterNearestMipmapLinear = 1 << 10,
-        MagFilterLinearMipmapLinear = 1 << 11,
-        HorizontalWrapClamp = 1 << 12,
-        HorizontalWrapRepeat = 1 << 13,
-        VerticalWrapClamp = 1 << 14,
-        VerticalWrapRepeat = 1 << 15,
-        DepthWrapClamp = 1 << 16,
-        DepthWrapRepeat = 1 << 17
+        static const uint32_t MinFilterNearest = 1 << 0;
+        static const uint32_t MinFilterLinear = 1 << 1;
+        static const uint32_t MinFilterNearestMipmapNearest = 1 << 2;
+        static const uint32_t MinFilterLinearMipmapNearest = 1 << 3;
+        static const uint32_t MinFilterNearestMipmapLinear = 1 << 4;
+        static const uint32_t MinFilterLinearMipmapLinear = 1 << 5;
+        static const uint32_t MagFilterNearest = 1 << 6;
+        static const uint32_t MagFilterLinear = 1 << 7;
+        static const uint32_t MagFilterNearestMipmapNearest = 1 << 8;
+        static const uint32_t MagFilterLinearMipmapNearest = 1 << 9;
+        static const uint32_t MagFilterNearestMipmapLinear = 1 << 10;
+        static const uint32_t MagFilterLinearMipmapLinear = 1 << 11;
+        static const uint32_t HorizontalWrapClamp = 1 << 12;
+        static const uint32_t HorizontalWrapRepeat = 1 << 13;
+        static const uint32_t VerticalWrapClamp = 1 << 14;
+        static const uint32_t VerticalWrapRepeat = 1 << 15;
+        static const uint32_t DepthWrapClamp = 1 << 16;
+        static const uint32_t DepthWrapRepeat = 1 << 17;
     };
 
     enum class FaceCull
