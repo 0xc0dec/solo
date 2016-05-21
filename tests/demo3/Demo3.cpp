1bad42700feafc130620e5d685e0bc36f7d0da46
Aleksey Fedotov <0xc0dec@gmail.com>
2016-05-21 14:11:18 +0500

    Left only text rendering in the demo3

--- tests/demo3/Demo3.cpp
+++ tests/demo3/Demo3.cpp
@@ -1,7 +1,6 @@
 #include "../../include/Solo.h"
 #include "../common/EscapeWatcher.h"
 #include "../common/Screenshoter.h"
-#include "../common/Shaders.h"
 #include <chrono>
 #include <iomanip>
 
@@ -50,7 +49,7 @@ public:
         initEngine();
         initCamera();
         initSkybox();
-        initMesh();
+//        initMesh();
         initText();
         device->run();
     }
@@ -105,53 +104,53 @@ public:
         auto textNode = scene->createNode();
         textNode->addComponent<Text>();
         auto transform = textNode->getComponent<Transform>();
-        transform->setLocalPosition(Vector3(-3.5f, 2, 0));
+        transform->setLocalPosition(Vector3(-3.5f, 0, 0));
         transform->setLocalScale(Vector3(0.02f, 0.02f, 1));
     }
 
-    void initMesh()
-    {
-        loader->loadRectTextureAsync("../assets/cobblestone.png")->done([=](sptr<RectTexture> tex)
-        {
-            tex->setWrapping(TextureWrapping::Clamp);
-            tex->generateMipmaps();
-            auto effect = Effect::create(commonShaders.vertex.basic, commonShaders.fragment.texture);
-            auto mat = Material::create(effect);
-            mat->setFaceCull(FaceCull::All);
-            mat->setPolygonMode(PolygonMode::Wireframe);
-            mat->bindWorldViewProjectionMatrixParameter("worldViewProjMatrix");
-            mat->setTextureParameter("mainTex", tex);
-
-            loader->loadMeshAsync("../assets/monkey.obj")->done([=](sptr<Mesh> mesh)
-            {
-                auto node = scene->createNode();
-                auto renderer = node->addComponent<MeshRenderer>();
-                renderer->setMesh(mesh);
-                renderer->setMaterial(0, mat);
-                node->getComponent<Transform>()->setLocalPosition(Vector3::zero());
-
-                // TODO move this to a separate demo
-                auto rigidBodyParams = RigidBodyConstructionParameters();
-                rigidBodyParams.mass = 10;
-                rigidBodyParams.restitution = 0.01f;
-                rigidBodyParams.friction = 0.01f;
-                auto rigidBody = node->addComponent<RigidBody>(rigidBodyParams);
-                rigidBody->setCollider(BoxCollider::create(Vector3::unit()));
-
-                node = scene->createNode();
-                renderer = node->addComponent<MeshRenderer>();
-                renderer->setMesh(mesh);
-                renderer->setMaterial(0, mat);
-                node->getComponent<Transform>()->setLocalPosition(Vector3(-1.5f, -5, 0));
-
-                // TODO move this to a separate demo
-                rigidBodyParams = RigidBodyConstructionParameters();
-                rigidBodyParams.mass = 0;
-                rigidBody = node->addComponent<RigidBody>(rigidBodyParams);
-                rigidBody->setCollider(BoxCollider::create(Vector3::unit()));
-            });
-        });
-    }
+//    void initMesh()
+//    {
+//        loader->loadRectTextureAsync("../assets/cobblestone.png")->done([=](sptr<RectTexture> tex)
+//        {
+//            tex->setWrapping(TextureWrapping::Clamp);
+//            tex->generateMipmaps();
+//            auto effect = Effect::create(commonShaders.vertex.basic, commonShaders.fragment.texture);
+//            auto mat = Material::create(effect);
+//            mat->setFaceCull(FaceCull::All);
+//            mat->setPolygonMode(PolygonMode::Wireframe);
+//            mat->bindWorldViewProjectionMatrixParameter("worldViewProjMatrix");
+//            mat->setTextureParameter("mainTex", tex);
+//
+//            loader->loadMeshAsync("../assets/monkey.obj")->done([=](sptr<Mesh> mesh)
+//            {
+//                auto node = scene->createNode();
+//                auto renderer = node->addComponent<MeshRenderer>();
+//                renderer->setMesh(mesh);
+//                renderer->setMaterial(0, mat);
+//                node->getComponent<Transform>()->setLocalPosition(Vector3::zero());
+//
+//                // TODO move this to a separate demo
+//                auto rigidBodyParams = RigidBodyConstructionParameters();
+//                rigidBodyParams.mass = 10;
+//                rigidBodyParams.restitution = 0.01f;
+//                rigidBodyParams.friction = 0.01f;
+//                auto rigidBody = node->addComponent<RigidBody>(rigidBodyParams);
+//                rigidBody->setCollider(BoxCollider::create(Vector3::unit()));
+//
+//                node = scene->createNode();
+//                renderer = node->addComponent<MeshRenderer>();
+//                renderer->setMesh(mesh);
+//                renderer->setMaterial(0, mat);
+//                node->getComponent<Transform>()->setLocalPosition(Vector3(-1.5f, -5, 0));
+//
+//                // TODO move this to a separate demo
+//                rigidBodyParams = RigidBodyConstructionParameters();
+//                rigidBodyParams.mass = 0;
+//                rigidBody = node->addComponent<RigidBody>(rigidBodyParams);
+//                rigidBody->setCollider(BoxCollider::create(Vector3::unit()));
+//            });
+//        });
+//    }
 
 private:
     Scene* scene = nullptr;
