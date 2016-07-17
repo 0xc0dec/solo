70da8c090073cdce7677d341cd9b5d6a735ce6c8
Aleksey Fedotov <0xc0dec@gmail.com>
2016-07-18 00:57:09 +0500

    Generate compilation error if vulkan renderer if not supported

--- src/platform/vulkan/SoloVulkanRenderer.cpp
+++ src/platform/vulkan/SoloVulkanRenderer.cpp
@@ -1,3 +1,11 @@
 #include "SoloVulkanRenderer.h"
 
 using namespace solo;
+
+#ifdef SL_VULKAN_RENDERER
+
+
+
+#else
+#   error Vulkan renderer is not supported on this platform
+#endif
--- src/platform/vulkan/SoloVulkanRenderer.h
+++ src/platform/vulkan/SoloVulkanRenderer.h
@@ -1,8 +1,16 @@
 #pragma once
 
+#include "SoloCommon.h"
+
+#ifdef SL_VULKAN_RENDERER
+
 namespace solo
 {
     class VulkanRenderer
     {
     };
 }
+
+#else
+#   error Vulkan renderer is not supported on this platform
+#endif
