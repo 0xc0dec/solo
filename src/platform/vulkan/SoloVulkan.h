b65646b664ae250d46b1a35010a6434b83c489d8
Aleksey Fedotov <0xc0dec@gmail.com>
2016-08-08 20:11:59 +0500

    Moved result-checking macro to common vk header

--- src/platform/vulkan/SoloVulkan.h
+++ src/platform/vulkan/SoloVulkan.h
@@ -6,3 +6,9 @@
 #   define VK_USE_PLATFORM_WIN32_KHR 
 #endif
 #include <vulkan.h>
+
+#ifdef SL_ERR_CHECK
+#   define SL_CHECK_VK_RESULT(result, ...) SL_DBG_BLOCK(if (result != VK_SUCCESS) SL_ERR(__VA_ARGS__))
+#else
+#   define SL_CHECK_VK_RESULT(result)
+#endif
--- src/platform/vulkan/SoloVulkanRenderer.cpp
+++ src/platform/vulkan/SoloVulkanRenderer.cpp
@@ -5,12 +5,6 @@
 
 using namespace solo;
 
-#ifdef SL_ERR_CHECK
-#   define SL_CHECK_VK_RESULT(result, ...) SL_DBG_BLOCK(if (result != VK_SUCCESS) SL_ERR(__VA_ARGS__))
-#else
-#   define SL_CHECK_VK_RESULT(result)
-#endif
-
 
 VkFormat getDepthFormat(VkPhysicalDevice device)
 {
