f4313c641c7f3f19c386dfc73fdb99f4fe5bff03
Aleksey Fedotov <0xc0dec@gmail.com>
2016-08-12 00:07:30 +0500

    Added missing impls

--- src/platform/stub/SoloStubDevice.h
+++ src/platform/stub/SoloStubDevice.h
@@ -7,7 +7,7 @@ namespace solo
     class StubDevice final: public Device
     {
     public:
-        explicit StubDevice(const DeviceSetup& setup);
+        explicit StubDevice(const DeviceSetup& setup): Device(setup) {}
 
         void setWindowTitle(const std::string& title) override final;
         auto getWindowTitle() const -> std::string override final;
@@ -17,8 +17,8 @@ namespace solo
         auto getLifetime() const -> float override final;
 
     private:
-        void beginUpdate() override final;
-        void endUpdate() override final;
+        void beginUpdate() override final {}
+        void endUpdate() override final {}
 
         std::string windowTitle;
     };
