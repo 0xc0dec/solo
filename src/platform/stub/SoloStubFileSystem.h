bd7bcff0c7d5b3e06591e8eb2866a4688dcaeccf
Aleksey Fedotov <0xc0dec@gmail.com>
2016-08-12 00:07:24 +0500

    Inlined StubFileSystem

--- src/platform/stub/SoloStubFileSystem.h
+++ src/platform/stub/SoloStubFileSystem.h
@@ -5,16 +5,41 @@
 
 namespace solo
 {
-    class StubFileSystem: public FileSystem
+    class StubFileSystem final: public FileSystem
     {
     public:
-        auto readBytes(const std::string& path) -> std::vector<uint8_t> override;
-        void writeBytes(const std::string& path, const std::vector<uint8_t>& data) override;
-
-        auto readText(const std::string& path) -> std::string override;
-
-        auto readLines(const std::string& path) -> std::vector<std::string> override;
-        void iterateLines(const std::string& path, std::function<bool(const std::string&)> process) override;
-        void writeLines(const std::string& path, const std::vector<std::string>& lines) override;
+        auto readBytes(const std::string& path) -> std::vector<uint8_t> override final;
+        void writeBytes(const std::string& path, const std::vector<uint8_t>& data) override final;
+        auto readText(const std::string& path) -> std::string override final;
+        auto readLines(const std::string& path) -> std::vector<std::string> override final;
+        void iterateLines(const std::string& path, std::function<bool(const std::string&)> process) override final;
+        void writeLines(const std::string& path, const std::vector<std::string>& lines) override final;
     };
+
+    inline auto StubFileSystem::readBytes(const std::string& path) -> std::vector<uint8_t>
+    {
+        return {};
+    }
+
+    inline void StubFileSystem::writeBytes(const std::string& path, const std::vector<uint8_t>& data)
+    {
+    }
+
+    inline auto StubFileSystem::readText(const std::string& path) -> std::string
+    {
+        return "";
+    }
+
+    inline auto StubFileSystem::readLines(const std::string& path) -> std::vector<std::string>
+    {
+        return {};
+    }
+
+    inline void StubFileSystem::iterateLines(const std::string& path, std::function<bool(const std::string&)> process)
+    {
+    }
+
+    inline void StubFileSystem::writeLines(const std::string& path, const std::vector<std::string>& lines)
+    {
+    }
 }
