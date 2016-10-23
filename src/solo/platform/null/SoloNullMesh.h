dc463b65b8ea995cba0d7b343a0612cf6d6da349
Aleksey Fedotov <0xc0dec@gmail.com>
2016-10-23 16:12:46 +0500

    License

--- src/solo/platform/null/SoloNullMesh.h
+++ src/solo/platform/null/SoloNullMesh.h
@@ -1,3 +1,23 @@
+/*
+    Copyright (c) Aleksey Fedotov
+
+    This software is provided 'as-is', without any express or implied
+    warranty. In no event will the authors be held liable for any damages
+    arising from the use of this software.
+
+    Permission is granted to anyone to use this software for any purpose,
+    including commercial applications, and to alter it and redistribute it
+    freely, subject to the following restrictions:
+
+    1. The origin of this software must not be misrepresented; you must not
+        claim that you wrote the original software. If you use this software
+        in a product, an acknowledgment in the product documentation would be
+        appreciated but is not required.
+    2. Altered source versions must be plainly marked as such, and must not be
+        misrepresented as being the original software.
+    3. This notice may not be removed or altered from any source distribution.
+*/
+
 #pragma once
 
 #include "SoloMesh.h"
@@ -5,7 +25,7 @@
 
 namespace solo
 {
-    class NullMesh: public Mesh
+    class NullMesh final: public Mesh
     {
     public:
         auto addVertexBuffer(const VertexBufferLayout& layout, const float* data, uint32_t vertexCount) -> uint32_t override final { return 0; }
