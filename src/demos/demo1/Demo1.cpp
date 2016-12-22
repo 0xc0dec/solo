6d8815a20e7b03b29f75158012cd54b620ee187c
Aleksey Fedotov <0xc0dec@gmail.com>
2016-12-22 22:43:00 +0000

    Added missing subsystem updates to Demo1

--- src/demos/demo1/Demo1.cpp
+++ src/demos/demo1/Demo1.cpp
@@ -383,6 +383,11 @@ int main()
     while (!device->isQuitRequested() && !device->isWindowCloseRequested() && !device->isKeyPressed(KeyCode::Escape, true))
     {
         device->beginUpdate();
+        device->getAssetLoader()->update();
+        device->getRenderer()->beginFrame();
+        device->getScene()->update();
+        device->getScene()->render();
+        device->getRenderer()->beginFrame();
         device->endUpdate();
     }
     return 0;
