4ec730ea3c2b48d9a883cc2d5e307c034f95136b
Aleksey Fedotov <0xc0dec@gmail.com>
2017-01-29 22:54:09 +0500

    Floor

--- src/demos/demo1.lua
+++ src/demos/demo1.lua
@@ -233,7 +233,25 @@ function initMonkeyHead(tex)
 end
 
 function initFloor(tex)
+    local material = solo.Material.create(dev, effects.simpleTexture)
+    material:setFaceCull(solo.FaceCull.All)
+    material:bindWorldViewProjectionMatrixParameter("worldViewProjMatrix")
+    material:setTextureParameter("mainTex", tex)
 
+    local node = scene:createNode()
+    
+    node:findComponent("Transform"):setLocalScale(solo.Vector3(10, 0.1, 10))
+    node:findComponent("Transform"):setLocalPosition(solo.Vector3(0, -2, 0))
+
+    local renderer = node:addComponent("MeshRenderer")
+    renderer:setMesh(meshes.cube)
+    renderer:setMaterial(0, material)
+
+    local params = solo.RigidBodyConstructionParameters()
+    params.mass = 0
+    params.friction = 0.5
+    local rigidBody = node:addComponent("RigidBody", params)
+    rigidBody:setCollider(solo.BoxCollider.create(solo.Vector3(1, 1, 1)))
 end
 
 mainCamera = initMainCamera()
@@ -244,6 +262,7 @@ initDynamicQuad()
 initCurrentTimeLabel()
 loadTextureAsync("../../assets/cobblestone.png", function(tex)
     initMonkeyHead(tex)
+    initFloor(tex)
 end)
 
 function keepRunning()
