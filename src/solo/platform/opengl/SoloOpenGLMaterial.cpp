6555d755ecd7405187bc5376dd1820c12a143a54
Aleksey Fedotov <alexey.fedotov@fastdev.se>
2017-01-30 18:32:35 +0500

    Moving material param functionality into material

--- src/solo/platform/opengl/SoloOpenGLMaterial.cpp
+++ src/solo/platform/opengl/SoloOpenGLMaterial.cpp
@@ -24,6 +24,7 @@
 
 #include "SoloDevice.h"
 #include "SoloOpenGLRenderer.h"
+#include "SoloOpenGLEffect.h"
 
 using namespace solo;
 
@@ -32,6 +33,7 @@ OpenGLMaterial::OpenGLMaterial(Device *device, sptr<Effect> effect):
     Material(device, effect)
 {
     renderer = dynamic_cast<OpenGLRenderer *>(device->getRenderer());
+    this->effect = std::dynamic_pointer_cast<OpenGLEffect>(effect);
 }
 
 
@@ -45,5 +47,174 @@ void OpenGLMaterial::applyState()
     renderer->setBlend(blend);
     renderer->setBlendFactor(srcBlendFactor, dstBlendFactor);
 }
+
+
+void OpenGLMaterial::applyParams()
+{
+    for (const auto &p: floatParams)
+        renderer->setUniform(uniformHandles[p.first], &p.second, 1); // TODO avoid handle lookup
+    // TODO etc...
+}
+
+
+void OpenGLMaterial::setFloatParameter2(const std::string &name, float value)
+{
+    if (floatParams.find(name) == floatParams.end())
+        uniformHandles[name] = renderer->createUniform(name.c_str(), UniformType::Float, effect->getHandle());
+    floatParams[name] = value;
+}
+
+
+void OpenGLMaterial::setFloatArrayParameter2(const std::string &name, const std::vector<float> &value)
+{
+    if (floatParams.find(name) == floatParams.end())
+        uniformHandles[name] = renderer->createUniform(name.c_str(), UniformType::FloatArray, effect->getHandle());
+    floatArrayParams[name] = value;
+}
+
+
+void OpenGLMaterial::setVector2Parameter2(const std::string& name, const Vector2 &value)
+{
+    if (vector2Params.find(name) == vector2Params.end())
+        uniformHandles[name] = renderer->createUniform(name.c_str(), UniformType::Vector2, effect->getHandle());
+    vector2Params[name] = value;
+}
+
+
+void OpenGLMaterial::setVector2ArrayParameter2(const std::string& name, const std::vector<Vector2> &value)
+{
+    if (vector2ArrayParams.find(name) == vector2ArrayParams.end())
+        uniformHandles[name] = renderer->createUniform(name.c_str(), UniformType::Vector2Array, effect->getHandle());
+    vector2ArrayParams[name] = value;
+}
+
+
+void OpenGLMaterial::setVector3Parameter2(const std::string &name, const Vector3 &value)
+{
+    if (vector3Params.find(name) == vector3Params.end())
+        uniformHandles[name] = renderer->createUniform(name.c_str(), UniformType::Vector3, effect->getHandle());
+    vector3Params[name] = value;
+}
+
+
+void OpenGLMaterial::setVector3ArrayParameter2(const std::string &name, const std::vector<Vector3> &value)
+{
+    if (vector3ArrayParams.find(name) == vector3ArrayParams.end())
+        uniformHandles[name] = renderer->createUniform(name.c_str(), UniformType::Vector3Array, effect->getHandle());
+    vector3ArrayParams[name] = value;
+}
+
+
+void OpenGLMaterial::setVector4Parameter2(const std::string &name, const Vector4 &value)
+{
+    if (vector4Params.find(name) == vector4Params.end())
+        uniformHandles[name] = renderer->createUniform(name.c_str(), UniformType::Vector4, effect->getHandle());
+    vector4Params[name] = value;
+}
+
+
+void OpenGLMaterial::setVector4ArrayParameter2(const std::string &name, const std::vector<Vector4> &value)
+{
+    if (vector4ArrayParams.find(name) == vector4ArrayParams.end())
+        uniformHandles[name] = renderer->createUniform(name.c_str(), UniformType::Vector4Array, effect->getHandle());
+    vector4ArrayParams[name] = value;
+}
+
+
+void OpenGLMaterial::setMatrixParameter2(const std::string &name, const Matrix& value)
+{
+    if (matrixParams.find(name) == matrixParams.end())
+        uniformHandles[name] = renderer->createUniform(name.c_str(), UniformType::Matrix, effect->getHandle());
+    matrixParams[name] = value;
+}
+
+
+void OpenGLMaterial::setMatrixArrayParameter2(const std::string &name, const std::vector<Matrix>& value)
+{
+    if (matrixArrayParams.find(name) == matrixArrayParams.end())
+        uniformHandles[name] = renderer->createUniform(name.c_str(), UniformType::MatrixArray, effect->getHandle());
+    matrixArrayParams[name] = value;
+}
+
+
+void OpenGLMaterial::setTextureParameter2(const std::string &name, sptr<Texture> value)
+{
+    if (textureParams.find(name) == textureParams.end())
+        uniformHandles[name] = renderer->createUniform(name.c_str(), UniformType::Texture, effect->getHandle());
+    textureParams[name] = value;
+}
+
+
+void OpenGLMaterial::bindWorldMatrixParameter2(const std::string& name)
+{
+    if (uniformHandles.find(name) == uniformHandles.end())
+        uniformHandles[name] = renderer->createUniform(name.c_str(), UniformType::Matrix, effect->getHandle());
+    worldMatrixParams.insert(name);
+}
+
+
+void OpenGLMaterial::bindViewMatrixParameter2(const std::string& name)
+{
+    if (uniformHandles.find(name) == uniformHandles.end())
+        uniformHandles[name] = renderer->createUniform(name.c_str(), UniformType::Matrix, effect->getHandle());
+    viewMatrixParams.insert(name);
+}
+
+
+void OpenGLMaterial::bindProjectionMatrixParameter2(const std::string& name)
+{
+    if (uniformHandles.find(name) == uniformHandles.end())
+        uniformHandles[name] = renderer->createUniform(name.c_str(), UniformType::Matrix, effect->getHandle());
+    projMatrixParams.insert(name);
+}
+
+
+void OpenGLMaterial::bindWorldViewMatrixParameter2(const std::string& name)
+{
+    if (uniformHandles.find(name) == uniformHandles.end())
+        uniformHandles[name] = renderer->createUniform(name.c_str(), UniformType::Matrix, effect->getHandle());
+    worldViewMatrixParams.insert(name);
+}
+
+
+void OpenGLMaterial::bindViewProjectionMatrixParameter2(const std::string& name)
+{
+    if (uniformHandles.find(name) == uniformHandles.end())
+        uniformHandles[name] = renderer->createUniform(name.c_str(), UniformType::Matrix, effect->getHandle());
+    viewProjMatrixParams.insert(name);
+}
+
+
+void OpenGLMaterial::bindWorldViewProjectionMatrixParameter2(const std::string& name)
+{
+    if (uniformHandles.find(name) == uniformHandles.end())
+        uniformHandles[name] = renderer->createUniform(name.c_str(), UniformType::Matrix, effect->getHandle());
+    worldViewProjMatrixParams.insert(name);
+}
+
+
+void OpenGLMaterial::bindInvTransposedWorldMatrixParameter2(const std::string& name)
+{
+    if (uniformHandles.find(name) == uniformHandles.end())
+        uniformHandles[name] = renderer->createUniform(name.c_str(), UniformType::Matrix, effect->getHandle());
+    invTransWorldMatrixParams.insert(name);
+}
+
+
+void OpenGLMaterial::bindInvTransposedWorldViewMatrixParameter2(const std::string& name)
+{
+    if (uniformHandles.find(name) == uniformHandles.end())
+        uniformHandles[name] = renderer->createUniform(name.c_str(), UniformType::Matrix, effect->getHandle());
+    invTransWorldViewMatrixParams.insert(name);
+}
+
+
+void OpenGLMaterial::bindCameraWorldPositionParameter2(const std::string& name)
+{
+    if (uniformHandles.find(name) == uniformHandles.end())
+        uniformHandles[name] = renderer->createUniform(name.c_str(), UniformType::Vector3, effect->getHandle());
+    camWorldPosParams.insert(name);
+}
+
 
 #endif
--- src/solo/platform/opengl/SoloOpenGLMaterial.h
+++ src/solo/platform/opengl/SoloOpenGLMaterial.h
@@ -25,10 +25,14 @@
 #ifdef SL_OPENGL_RENDERER
 
 #include "SoloMaterial.h"
+#include <unordered_map>
+#include <unordered_set>
 
 namespace solo
 {
     class OpenGLRenderer;
+    class OpenGLEffect;
+    class OpenGLTexture;
     class Device;
 
     class OpenGLMaterial final: public Material
@@ -36,10 +40,67 @@ namespace solo
     public:
         explicit OpenGLMaterial(Device *device, sptr<Effect> effect);
 
+        void setFloatParameter2(const std::string &name, float value);
+        void setFloatArrayParameter2(const std::string &name, const std::vector<float> &value);
+
+        void setVector2Parameter2(const std::string &name, const Vector2 &value);
+        void setVector2ArrayParameter2(const std::string &name, const std::vector<Vector2> &value);
+
+        void setVector3Parameter2(const std::string &name, const Vector3 &value);
+        void setVector3ArrayParameter2(const std::string &name, const std::vector<Vector3> &value);
+
+        void setVector4Parameter2(const std::string &name, const Vector4 &value);
+        void setVector4ArrayParameter2(const std::string &name, const std::vector<Vector4> &value);
+
+        void setMatrixParameter2(const std::string &name, const Matrix &value);
+        void setMatrixArrayParameter2(const std::string &name, const std::vector<Matrix> &value);
+
+        void setTextureParameter2(const std::string &name, sptr<Texture> value);
+
+        void bindWorldMatrixParameter2(const std::string &name);
+        void bindViewMatrixParameter2(const std::string &name);
+        void bindProjectionMatrixParameter2(const std::string &name);
+        void bindWorldViewMatrixParameter2(const std::string &name);
+        void bindViewProjectionMatrixParameter2(const std::string &name);
+        void bindWorldViewProjectionMatrixParameter2(const std::string &name);
+        void bindInvTransposedWorldMatrixParameter2(const std::string &name);
+        void bindInvTransposedWorldViewMatrixParameter2(const std::string &name);
+        void bindCameraWorldPositionParameter2(const std::string &name);
+
     protected:
         void applyState() override final;
+        void applyParams();
 
         OpenGLRenderer *renderer = nullptr;
+        sptr<OpenGLEffect> effect = nullptr;
+
+        template <class T>
+        using map = std::unordered_map<std::string, T>;
+        using strSet = std::unordered_set<std::string>;
+
+        map<uint32_t> uniformHandles;
+
+        map<float> floatParams;
+        map<Vector2> vector2Params;
+        map<Vector3> vector3Params;
+        map<Vector4> vector4Params;
+        map<Matrix> matrixParams;
+        map<std::vector<float>> floatArrayParams;
+        map<std::vector<Vector2>> vector2ArrayParams;
+        map<std::vector<Vector3>> vector3ArrayParams;
+        map<std::vector<Vector4>> vector4ArrayParams;
+        map<std::vector<Matrix>> matrixArrayParams;
+        map<sptr<OpenGLTexture>> textureParams;
+
+        strSet worldMatrixParams;
+        strSet viewMatrixParams;
+        strSet projMatrixParams;
+        strSet worldViewMatrixParams;
+        strSet viewProjMatrixParams;
+        strSet worldViewProjMatrixParams;
+        strSet invTransWorldMatrixParams;
+        strSet invTransWorldViewMatrixParams;
+        strSet camWorldPosParams;
     };
 }
 
