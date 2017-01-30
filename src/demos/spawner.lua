fa0e62bf34515b3f764831ea0c720d4e156e406f
Aleksey Fedotov <alexey.fedotov@fastdev.se>
2017-01-30 11:34:22 +0500

    Use new vector ops

--- src/demos/spawner.lua
+++ src/demos/spawner.lua
@@ -11,12 +11,7 @@ return function(device, mesh, effect)
 
         update = function(self)
             if device:isKeyPressed(solo.KeyCode.Space, true) then
-                local localPos = self.transform:getLocalPosition()
-                local localForward = self.transform:getLocalForward()
-                local initialPos = solo.Vector3(
-                    localPos.x + localForward.x * 2,
-                    localPos.y + localForward.y * 2,
-                    localPos.z + localForward.z * 2)
+                local initialPos = self.transform:getLocalPosition() + self.transform:getLocalForward() * 2
                 local initialRotation = self.transform:getLocalRotation()
                 self.scene:createNode():addScriptComponent(
                     createSpawnedObject(device, effect, mesh, initialPos, initialRotation))
