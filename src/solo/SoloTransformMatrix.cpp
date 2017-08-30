b98914fe9baee4d5afafec8f46ecdae5213956b7
Aleksey Fedotov <alexey.fedotov@fastdev.se>
2017-08-30 13:03:45 +0500

    glm versions of TransformMatrix methods

--- src/solo/SoloTransformMatrix.cpp
+++ src/solo/SoloTransformMatrix.cpp
@@ -85,6 +85,26 @@ auto TransformMatrix::createPerspective(const Radian &fieldOfView, float aspectR
     return TransformMatrix(result);
 }
 
+auto TransformMatrix::createPerspective2(float fieldOfView, float aspectRatio, float znear, float zfar) -> TransformMatrix
+{
+    auto f_n = 1.0f / (zfar - znear);
+    auto theta = fieldOfView * 0.5f;
+    SL_PANIC_IF(math::isZero(fmod(theta, math::piOver2), math::epsilon1))
+
+    auto divisor = tan(theta);
+    auto factor = 1.0f / divisor;
+
+    Matrix result;
+    memset(&result.m, 0, getSize());
+    result.m[0] = (1.0f / aspectRatio) * factor;
+    result.m[5] = factor;
+    result.m[10] = -(zfar + znear) * f_n;
+    result.m[11] = -1.0f;
+    result.m[14] = -2.0f * zfar * znear * f_n;
+
+    return TransformMatrix(result);
+}
+
 auto TransformMatrix::createOrthographic(float width, float height, float near, float far) -> TransformMatrix
 {
     auto halfWidth = width / 2.0f;
@@ -114,7 +134,57 @@ auto TransformMatrix::createScale(const Vector3 &scale) -> TransformMatrix
     return TransformMatrix(result);
 }
 
+auto TransformMatrix::createScale2(const glm::vec3 &scale) -> TransformMatrix
+{
+    Matrix result;
+    result.m[0] = scale.x;
+    result.m[5] = scale.y;
+    result.m[10] = scale.z;
+    return TransformMatrix(result);
+}
+
 auto TransformMatrix::createRotationFromQuaternion(const Quaternion &q) -> TransformMatrix
+{
+    auto x2 = q.x + q.x;
+    auto y2 = q.y + q.y;
+    auto z2 = q.z + q.z;
+
+    auto xx2 = q.x * x2;
+    auto yy2 = q.y * y2;
+    auto zz2 = q.z * z2;
+    auto xy2 = q.x * y2;
+    auto xz2 = q.x * z2;
+    auto yz2 = q.y * z2;
+    auto wx2 = q.w * x2;
+    auto wy2 = q.w * y2;
+    auto wz2 = q.w * z2;
+
+    Matrix result;
+
+    result.m[0] = 1.0f - yy2 - zz2;
+    result.m[1] = xy2 + wz2;
+    result.m[2] = xz2 - wy2;
+    result.m[3] = 0.0f;
+
+    result.m[4] = xy2 - wz2;
+    result.m[5] = 1.0f - xx2 - zz2;
+    result.m[6] = yz2 + wx2;
+    result.m[7] = 0.0f;
+
+    result.m[8] = xz2 + wy2;
+    result.m[9] = yz2 - wx2;
+    result.m[10] = 1.0f - xx2 - yy2;
+    result.m[11] = 0.0f;
+
+    result.m[12] = 0.0f;
+    result.m[13] = 0.0f;
+    result.m[14] = 0.0f;
+    result.m[15] = 1.0f;
+
+    return TransformMatrix(result);
+}
+
+auto TransformMatrix::createRotationFromQuaternion2(const glm::quat &q) -> TransformMatrix
 {
     auto x2 = q.x + q.x;
     auto y2 = q.y + q.y;
@@ -216,6 +286,67 @@ auto TransformMatrix::createRotationFromAxisAngle(const Vector3 &axis, const Rad
     return TransformMatrix(result);
 }
 
+auto TransformMatrix::createRotationFromAxisAngle2(const glm::vec3 &axis, float angle) -> TransformMatrix
+{
+    auto x = axis.x;
+    auto y = axis.y;
+    auto z = axis.z;
+
+    // Make sure the input axis is normalized
+    auto n = x * x + y * y + z * z;
+    if (n != 1.0f)
+    {
+        // Not normalized
+        n = sqrtf(n);
+        // Prevent division too close to zero.
+        if (!math::isZero(n, math::epsilon1))
+        {
+            n = 1.0f / n;
+            x *= n;
+            y *= n;
+            z *= n;
+        }
+    }
+
+    auto c = cosf(angle);
+    auto s = sinf(angle);
+
+    auto t = 1.0f - c;
+    auto tx = t * x;
+    auto ty = t * y;
+    auto tz = t * z;
+    auto txy = tx * y;
+    auto txz = tx * z;
+    auto tyz = ty * z;
+    auto sx = s * x;
+    auto sy = s * y;
+    auto sz = s * z;
+
+    Matrix result;
+
+    result.m[0] = c + tx * x;
+    result.m[1] = txy + sz;
+    result.m[2] = txz - sy;
+    result.m[3] = 0.0f;
+
+    result.m[4] = txy - sz;
+    result.m[5] = c + ty * y;
+    result.m[6] = tyz + sx;
+    result.m[7] = 0.0f;
+
+    result.m[8] = txz + sy;
+    result.m[9] = tyz - sx;
+    result.m[10] = c + tz * z;
+    result.m[11] = 0.0f;
+
+    result.m[12] = 0.0f;
+    result.m[13] = 0.0f;
+    result.m[14] = 0.0f;
+    result.m[15] = 1.0f;
+
+    return TransformMatrix(result);
+}
+
 auto TransformMatrix::createRotationX(const Radian &angle) -> TransformMatrix
 {
     auto c = cosf(angle.toRawRadian());
@@ -229,10 +360,39 @@ auto TransformMatrix::createRotationX(const Radian &angle) -> TransformMatrix
     return TransformMatrix(result);
 }
 
+auto TransformMatrix::createRotationX2(float angle) -> TransformMatrix
+{
+    auto c = cosf(angle);
+    auto s = sinf(angle);
+
+    Matrix result;
+    result.m[5] = c;
+    result.m[6] = s;
+    result.m[9] = -s;
+    result.m[10] = c;
+
+    return TransformMatrix(result);
+}
+
 auto TransformMatrix::createRotationY(const Radian &angle) -> TransformMatrix
 {
     auto c = cosf(angle.toRawRadian());
     auto s = sinf(angle.toRawRadian());
+
+    Matrix result;
+
+    result.m[0] = c;
+    result.m[2] = -s;
+    result.m[8] = s;
+    result.m[10] = c;
+
+    return TransformMatrix(result);
+}
+
+auto TransformMatrix::createRotationY2(float angle) -> TransformMatrix
+{
+    auto c = cosf(angle);
+    auto s = sinf(angle);
 
     Matrix result;
 
@@ -257,7 +417,30 @@ auto TransformMatrix::createRotationZ(const Radian &angle) -> TransformMatrix
     return TransformMatrix(result);
 }
 
+auto TransformMatrix::createRotationZ2(float angle) -> TransformMatrix
+{
+    auto c = cosf(angle);
+    auto s = sinf(angle);
+
+    Matrix result;
+    result.m[0] = c;
+    result.m[1] = s;
+    result.m[4] = -s;
+    result.m[5] = c;
+
+    return TransformMatrix(result);
+}
+
 auto TransformMatrix::createTranslation(const Vector3 &translation) -> TransformMatrix
+{
+    Matrix result;
+    result.m[12] = translation.x;
+    result.m[13] = translation.y;
+    result.m[14] = translation.z;
+    return TransformMatrix(result);
+}
+
+auto TransformMatrix::createTranslation2(const glm::vec3 &translation) -> TransformMatrix
 {
     Matrix result;
     result.m[12] = translation.x;
@@ -273,10 +456,24 @@ auto TransformMatrix::getScale() const -> Vector3
     return result;
 }
 
+auto TransformMatrix::getScale2() const -> glm::vec3
+{
+    glm::vec3 result;
+    decompose2(&result, nullptr, nullptr);
+    return result;
+}
+
 auto TransformMatrix::getRotation() const -> Quaternion
 {
     Quaternion result;
     decompose(nullptr, &result, nullptr);
+    return result;
+}
+
+auto TransformMatrix::getRotation2() const -> glm::quat
+{
+    glm::quat result;
+    decompose2(nullptr, &result, nullptr);
     return result;
 }
 
@@ -287,12 +484,23 @@ auto TransformMatrix::getTranslation() const -> Vector3
     return result;
 }
 
-void TransformMatrix::scaleByVector(const Vector3 &s)
+auto TransformMatrix::getTranslation2() const -> glm::vec3
 {
-    *this *= createScale(s);
+    glm::vec3 result;
+    decompose2(nullptr, nullptr, &result);
+    return result;
 }
 
 auto TransformMatrix::transformPoint(const Vector3 &point) const -> Vector3
+{
+    return {
+        point.x * m[0] + point.y * m[4] + point.z * m[8] + m[12],
+        point.x * m[1] + point.y * m[5] + point.z * m[9] + m[13],
+        point.x * m[2] + point.y * m[6] + point.z * m[10] + m[14]
+    };
+}
+
+auto TransformMatrix::transformPoint2(const glm::vec3 &point) const -> glm::vec3
 {
     return {
         point.x * m[0] + point.y * m[4] + point.z * m[8] + m[12],
@@ -310,9 +518,13 @@ auto TransformMatrix::transformDirection(const Vector3 &dir) const -> Vector3
     };
 }
 
-void TransformMatrix::translate(const Vector3 &t)
+auto TransformMatrix::transformDirection2(const glm::vec3 &dir) const -> glm::vec3
 {
-    *this *= createTranslation(t);
+    return{
+        dir.x * m[0] + dir.y * m[4] + dir.z * m[8],
+        dir.x * m[1] + dir.y * m[5] + dir.z * m[9],
+        dir.x * m[2] + dir.y * m[6] + dir.z * m[10]
+    };
 }
 
 static void updateBounds(const Vector3 &point, Vector3 &min, Vector3 &max)
@@ -394,6 +606,109 @@ auto TransformMatrix::transformRay(const Ray &ray) -> Ray
 }
 
 bool TransformMatrix::decompose(Vector3 *scale, Quaternion *rotation, Vector3 *translation) const
+{
+    if (translation)
+    {
+        translation->x = m[12];
+        translation->y = m[13];
+        translation->z = m[14];
+    }
+
+    if (scale == nullptr && rotation == nullptr)
+        return true;
+
+    Vector3 xaxis(m[0], m[1], m[2]);
+    auto scaleX = xaxis.length();
+
+    Vector3 yaxis(m[4], m[5], m[6]);
+    auto scaleY = yaxis.length();
+
+    Vector3 zaxis(m[8], m[9], m[10]);
+    auto scaleZ = zaxis.length();
+
+    // Determine if we have a negative scale (true if determinant is less than zero).
+    // In this case, we simply negate a single axis of the scale.
+    auto det = getDeterminant();
+    if (det < 0)
+        scaleZ = -scaleZ;
+
+    if (scale)
+    {
+        scale->x = scaleX;
+        scale->y = scaleY;
+        scale->z = scaleZ;
+    }
+
+    if (rotation == nullptr)
+        return true;
+
+    // Scale too close to zero, can't decompose rotation.
+    if (scaleX < math::epsilon3 || scaleY < math::epsilon3 || scaleZ < math::epsilon3)
+        return false;
+
+    float rn;
+
+    // Factor the scale out of the matrix axes.
+    rn = 1.0f / scaleX;
+    xaxis.x *= rn;
+    xaxis.y *= rn;
+    xaxis.z *= rn;
+
+    rn = 1.0f / scaleY;
+    yaxis.x *= rn;
+    yaxis.y *= rn;
+    yaxis.z *= rn;
+
+    rn = 1.0f / scaleZ;
+    zaxis.x *= rn;
+    zaxis.y *= rn;
+    zaxis.z *= rn;
+
+    // Calculate the rotation from the resulting matrix (axes).
+    auto trace = xaxis.x + yaxis.y + zaxis.z + 1.0f;
+
+    if (trace > math::epsilon1)
+    {
+        auto s = 0.5f / sqrt(trace);
+        rotation->w = 0.25f / s;
+        rotation->x = (yaxis.z - zaxis.y) * s;
+        rotation->y = (zaxis.x - xaxis.z) * s;
+        rotation->z = (xaxis.y - yaxis.x) * s;
+    }
+    else
+    {
+        // Note: since xaxis, yaxis, and zaxis are normalized,
+        // we will never divide by zero in the code below.
+        if (xaxis.x > yaxis.y && xaxis.x > zaxis.z)
+        {
+            auto s = 0.5f / sqrt(1.0f + xaxis.x - yaxis.y - zaxis.z);
+            rotation->w = (yaxis.z - zaxis.y) * s;
+            rotation->x = 0.25f / s;
+            rotation->y = (yaxis.x + xaxis.y) * s;
+            rotation->z = (zaxis.x + xaxis.z) * s;
+        }
+        else if (yaxis.y > zaxis.z)
+        {
+            auto s = 0.5f / sqrt(1.0f + yaxis.y - xaxis.x - zaxis.z);
+            rotation->w = (zaxis.x - xaxis.z) * s;
+            rotation->x = (yaxis.x + xaxis.y) * s;
+            rotation->y = 0.25f / s;
+            rotation->z = (zaxis.y + yaxis.z) * s;
+        }
+        else
+        {
+            auto s = 0.5f / sqrt(1.0f + zaxis.z - xaxis.x - yaxis.y);
+            rotation->w = (xaxis.y - yaxis.x) * s;
+            rotation->x = (zaxis.x + xaxis.z) * s;
+            rotation->y = (zaxis.y + yaxis.z) * s;
+            rotation->z = 0.25f / s;
+        }
+    }
+
+    return true;
+}
+
+bool TransformMatrix::decompose2(glm::vec3 *scale, glm::quat *rotation, glm::vec3 *translation) const
 {
     if (translation)
     {
