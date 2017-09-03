6a543ca03993b5b81ce0c577b91b27d8c03511a2
Aleksey Fedotov <0xc0dec@gmail.com>
2017-09-03 19:55:26 +0500

    Fixed missing arg

--- src/demos/demo1/PostProcessors.lua
+++ src/demos/demo1/PostProcessors.lua
@@ -93,7 +93,7 @@ function createPostProcessor2(camera, tag, effects)
     local stitchWidth = 30
     local canvasSize = sl.device:getCanvasSize()
 
-    local stitchTex = sl.RectTexture.loadFromFile(getAssetPath("Stitches.png"))
+    local stitchTex = sl.RectTexture.loadFromFile(sl.device, getAssetPath("Stitches.png"))
     stitchTex:setFiltering(sl.TextureFiltering.Nearest)
 
     local stitchTexSize = stitchTex:getSize()
