5b7f7cdc89b0a5a2a8e8e9de7efd8b817cd6def8
Aleksey Fedotov <0xc0dec@gmail.com>
2017-09-11 01:59:14 +0500

    Demo 1 refactoring part 3

--- src/demos/demo1/Demo1.lua
+++ src/demos/demo1/Demo1.lua
@@ -17,16 +17,11 @@ local scene = sl.Scene.create(dev)
 local canvasSize = dev:getCanvasSize()
 
 local postProcessors = require "PostProcessors"
-local createEffects = require "Effects"
-local createMainCamera = require "MainCamera"
-local createOffscreenCamera = require "OffscreenCamera"
-local createSkybox = require "Skybox"
-local createCheckerBox = require "CheckerBox"
-local createDynamicQuad = require "DynamicQuad"
-local createTransparentQuad = require "TransparentQuad"
-local createMonitorQuad = require "MonitorQuad"
-local createRotatingMesh = require "RotatingMesh"
-local attachAxesMesh = require "Axes"
+local createRotator = require "Rotator"
+local createScreenshoter = require "Screenshoter"
+local createSpawner = require "Spawner"
+local createSpawnedObjectTargeter = require "SpawnedObjectTargeter"
+local createLookAt = require "LookAt"
 
 local loadTexture2d = function(path)
     local tex = sl.Texture2d.loadFromFile(dev, path)
@@ -34,6 +29,263 @@ local loadTexture2d = function(path)
     tex:setFiltering(sl.TextureFiltering.LinearMipmapNearest)
     tex:setAnisotropyLevel(8)
     return tex
+end
+
+local createTransparentQuad = function(effects, quadMesh, tag, tex)
+    tex:setWrapping(sl.TextureWrapping.Clamp)
+
+    local material = sl.Material.create(sl.device, effects.simpleTexture)
+    material:setFaceCull(sl.FaceCull.All)
+    material:bindParameter("worldViewProjMatrix", sl.BindParameterSemantics.WorldViewProjectionMatrix)
+    material:setTextureParameter("mainTex", tex)
+    material:setBlend(true)
+    material:setDepthTest(true)
+    material:setDepthWrite(false)
+
+    local parent = scene:createNode()
+    parent:findComponent("Transform"):setLocalPosition(vec3(5, 0, 0))
+    parent:addScriptComponent(createRotator("world", vec3(0, 1, 0)))
+
+    local node = scene:createNode()
+    node:addScriptComponent(createRotator("local", vec3(1, 0, 0)))
+    node:findComponent("Transform"):setParent(parent:findComponent("Transform"))
+    node:findComponent("Transform"):setLocalPosition(vec3(2, 0, 0))
+
+    local renderer = node:addComponent("MeshRenderer")
+    renderer:setMesh(quadMesh)
+    renderer:setMaterial(0, material)
+    renderer:setTag(tag)
+
+    return parent
+end
+
+local attachAxesMesh = function(effects, node, mesh)
+	-- TODO cache materials
+	local createColorMaterial = function(color)
+	    local mat = sl.Material.create(sl.device, effects.color)
+	    mat:setFaceCull(sl.FaceCull.All)
+	    mat:bindParameter("worldViewProjMatrix", sl.BindParameterSemantics.WorldViewProjectionMatrix)
+	    mat:setVector4Parameter("color", color)
+	    return mat
+	end
+
+	local materials = {
+	    red = createColorMaterial(vec4(1, 0, 0, 1)),
+	    green = createColorMaterial(vec4(0, 1, 0, 1)),
+	    blue = createColorMaterial(vec4(0, 0, 1, 1)),
+	    white = createColorMaterial(vec4(1, 1, 1, 1))
+	}
+
+	local renderer = node:addComponent("MeshRenderer")
+    renderer:setMesh(mesh)
+    renderer:setMaterial(0, materials.blue)
+    renderer:setMaterial(1, materials.green)
+    renderer:setMaterial(2, materials.white)
+    renderer:setMaterial(3, materials.red)
+end
+
+
+local createMonitorQuad = function(effects, offscreenCameraTex, quadMesh, tag)
+	local canvasSize = sl.device:getCanvasSize()
+
+    local material = sl.Material.create(sl.device, effects.simpleTexture)
+    material:setFaceCull(sl.FaceCull.All)
+    material:bindParameter("worldViewProjMatrix", sl.BindParameterSemantics.WorldViewProjectionMatrix)
+    material:setTextureParameter("mainTex", offscreenCameraTex)
+
+    local parent = scene:createNode()
+    parent:findComponent("Transform"):setLocalPosition(vec3(-2, 2, -2))
+    parent:addScriptComponent(createRotator("world", vec3(0, 1, 0)))
+
+    local node = scene:createNode()
+    node:addScriptComponent(createLookAt(vec3(0, 0, 0)))
+
+    local transform = node:findComponent("Transform")
+    transform:setParent(parent:findComponent("Transform"))
+    transform:setLocalPosition(vec3(5, 2, -5))
+    transform:setLocalScale(vec3(5, 5 * canvasSize.y / canvasSize.x, 1))
+    
+    local renderer = node:addComponent("MeshRenderer")
+    renderer:setMesh(quadMesh)
+    renderer:setMaterial(0, material)
+    renderer:setTag(tag)
+
+    return parent
+end
+
+local createSkybox = function(tag)
+    local getImagePath = function(fileName) return getAssetPath("textures/skyboxes/deep-space/") .. fileName end
+    local tex = sl.CubeTexture.loadFromFaceFiles(sl.device,
+        getImagePath("Front.png"), getImagePath("Back.png"), getImagePath("Left.png"),
+        getImagePath("Right.png"), getImagePath("Top.png"), getImagePath("Bottom.png")
+    )
+    tex:setWrapping(sl.TextureWrapping.Clamp)
+    tex:setFiltering(sl.TextureFiltering.Linear)
+    local node = scene:createNode()
+    local renderer = node:addComponent("SkyboxRenderer")
+    renderer:setTexture(tex)
+    renderer:setTag(tag)
+end
+
+local createMainCamera = function(meshes, effects)
+    local node = scene:createNode()
+        
+    local t = node:findComponent("Transform")
+    t:setLocalPosition(vec3(0, 5, 10))
+    t:lookAt(vec3(0, 0, 0), vec3(0, 1, 0))
+    
+    node:addComponent("Spectator")
+    node:addScriptComponent(createScreenshoter("Demo1.bmp"))
+    node:addScriptComponent(createSpawnedObjectTargeter(physics))
+    node:addScriptComponent(createSpawner(meshes.cube, effects.color))
+
+    local cam = node:addComponent("Camera")
+    cam:setClearColor(vec4(0.0, 0.6, 0.6, 1.0))
+    cam:setNear(0.05)
+
+    return cam
+end
+
+local createEffects = function()
+    local shaderFiles = {
+        vs = {
+            basic = getAssetPath("shaders/gl/Basic.vert.glsl"),
+            passThrough = getAssetPath("shaders/gl/PassThrough.vert.glsl"),
+            basicLighting = getAssetPath("shaders/gl/BasicLighting.vert.glsl")
+        },
+
+        fs = {
+            checker = getAssetPath("shaders/gl/Checker.frag.glsl"),
+            textureWithLighting = getAssetPath("shaders/gl/TextureWithLighting.frag.glsl"),
+            grayscale = getAssetPath("shaders/gl/Grayscale.frag.glsl"),
+            saturate = getAssetPath("shaders/gl/Saturate.frag.glsl"),
+            verticalBlur = getAssetPath("shaders/gl/VerticalBlur.frag.glsl"),
+            horizontalBlur = getAssetPath("shaders/gl/HorizontalBlur.frag.glsl"),
+            texture = getAssetPath("shaders/gl/Texture.frag.glsl"),
+            color = getAssetPath("shaders/gl/Color.frag.glsl"),
+            stitches = getAssetPath("shaders/gl/Stitches.frag.glsl")
+        }
+    }
+
+    return {
+        simpleTexture = sl.Effect.loadFromFiles(sl.device, shaderFiles.vs.basic, shaderFiles.fs.texture),
+        color = sl.Effect.loadFromFiles(sl.device, shaderFiles.vs.basic, shaderFiles.fs.color),
+        checker = sl.Effect.loadFromFiles(sl.device, shaderFiles.vs.basic, shaderFiles.fs.checker),
+        basicLighting = sl.Effect.loadFromFiles(sl.device, shaderFiles.vs.basicLighting, shaderFiles.fs.textureWithLighting),
+        grayscale = sl.Effect.loadFromFiles(sl.device, shaderFiles.vs.passThrough, shaderFiles.fs.grayscale),
+        saturate = sl.Effect.loadFromFiles(sl.device, shaderFiles.vs.passThrough, shaderFiles.fs.saturate),
+        verticalBlur = sl.Effect.loadFromFiles(sl.device, shaderFiles.vs.passThrough, shaderFiles.fs.verticalBlur),
+        horizontalBlur = sl.Effect.loadFromFiles(sl.device, shaderFiles.vs.passThrough, shaderFiles.fs.horizontalBlur),
+        stitches = sl.Effect.loadFromFiles(sl.device, shaderFiles.vs.passThrough, shaderFiles.fs.stitches)
+    }
+end
+
+local createDynamicQuad = function(effects, loadTexture)
+    local createUpdater = function(data, mesh)
+        local time = 0
+
+        return {
+            typeId = sl.getCmpId("DynamicQuadUpdater"),
+
+            update = function()
+                time = time + 2 * sl.device:getTimeDelta()
+                local offset = 0.5 * math.sin(time)
+                data[3] = offset
+                data[8] = -offset
+                data[13] = offset
+                data[18] = -offset
+                mesh:updateDynamicVertexBuffer(0, 0, data, 4)
+            end
+        }
+    end
+
+    local tex = loadTexture(getAssetPath("textures/Bricks.jpg"))
+    tex:setWrapping(sl.TextureWrapping.Clamp)
+
+    local layout = sl.VertexBufferLayout()
+    layout:addAttribute(3, 0)
+    layout:addAttribute(2, 1)
+
+    local data = {
+        -1, -1, 0,    0, 0,
+        -1,  1, 0,    0, 1,
+        1,  1, 0,    1, 1,
+        1, -1, 0,    1, 0
+    }
+
+    local indices = {
+        0, 1, 2,
+        0, 2, 3
+    }
+
+    local mesh = sl.Mesh.create(sl.device)
+    mesh:addDynamicVertexBuffer(layout, data, 4)
+    mesh:addPart(indices, 6)
+    mesh:setPrimitiveType(sl.PrimitiveType.Triangles)
+
+    local material = sl.Material.create(sl.device, effects.simpleTexture)
+    material:setFaceCull(sl.FaceCull.All)
+    material:bindParameter("worldViewProjMatrix", sl.BindParameterSemantics.WorldViewProjectionMatrix)
+    material:setTextureParameter("mainTex", tex)
+
+    local node = scene:createNode()
+    node:findComponent("Transform"):setLocalPosition(vec3(0, 0, -5))
+    local renderer = node:addComponent("MeshRenderer")
+    renderer:setMesh(mesh)
+    renderer:setMaterial(0, material)
+
+    node:addScriptComponent(createUpdater(data, mesh))
+end
+
+local createCheckerBox = function(effects, cubeMesh)
+    local material = sl.Material.create(sl.device, effects.checker)
+    material:setFaceCull(sl.FaceCull.All)
+    material:bindParameter("worldViewProjMatrix", sl.BindParameterSemantics.WorldViewProjectionMatrix)
+    material:setVector4Parameter("color", vec4(1, 1, 0, 1))
+
+    local node = scene:createNode()
+    node:addComponent("MeshRenderer"):setMesh(cubeMesh)
+    node:findComponent("MeshRenderer"):setMaterial(0, material)
+    node:findComponent("Transform"):setLocalPosition(vec3(-5, 0, 0))
+    node:addScriptComponent(createRotator("world", vec3(0, 1, 0)))
+end
+
+local createRotatingMesh = function(effects, tex, mesh)
+    local material = sl.Material.create(sl.device, effects.basicLighting)
+    material:setFaceCull(sl.FaceCull.All)
+    material:bindParameter("worldViewProjMatrix", sl.BindParameterSemantics.WorldViewProjectionMatrix)
+    material:bindParameter("invTransposedWorldMatrix", sl.BindParameterSemantics.InverseTransposedWorldMatrix)
+    material:setTextureParameter("mainTex", tex)
+
+    local node = scene:createNode()
+    local renderer = node:addComponent("MeshRenderer")
+    renderer:setMesh(mesh)
+    renderer:setMaterial(0, material)
+    node:findComponent("Transform"):setLocalPosition(vec3(0, 0, 0))
+    node:addScriptComponent(createRotator("local", vec3(1, 0, 0)))
+end
+
+local createOffscreenCamera = function()
+	local canvasSize = sl.device:getCanvasSize()
+
+    local tex = sl.Texture2d.createEmpty(sl.device,
+        math.floor(canvasSize.x / 8.0), math.floor(canvasSize.y / 8.0), sl.TextureFormat.RGB)
+    tex:setFiltering(sl.TextureFiltering.Nearest)
+    tex:setWrapping(sl.TextureWrapping.Clamp)
+
+    local node = scene:createNode()
+    node:findComponent("Transform"):setLocalPosition(vec3(0, 0, 10))
+
+    local cam = node:addComponent("Camera")
+    cam:setClearColor(vec4(1, 0, 1, 1))
+    cam:setNear(0.05)
+    cam:setViewport(vec4(0, 0, canvasSize.x / 8, canvasSize.y / 8))
+
+    local fb = sl.FrameBuffer.create(sl.device)
+    fb:setAttachments({ tex })
+    cam:setRenderTarget(fb)
+
+    return cam, tex
 end
 
 local createFloor = function(effects, tex, cubeMesh)
@@ -107,26 +359,26 @@ local meshes = {
     quad = sl.Mesh.createFromPrefab(dev, sl.MeshPrefab.Quad)
 }
 
-local mainCamera = createMainCamera(scene, physics, meshes, effects)
-local offscreenCamera, offscreenCameraTex = createOffscreenCamera(scene)
-createSkybox(scene, knownTags.skybox)
-createCheckerBox(scene, effects, meshes.cube)
-createDynamicQuad(scene, effects, loadTexture2d)
+local mainCamera = createMainCamera(meshes, effects)
+local offscreenCamera, offscreenCameraTex = createOffscreenCamera()
+createSkybox(knownTags.skybox)
+createCheckerBox(effects, meshes.cube)
+createDynamicQuad(effects, loadTexture2d)
 createTimeLabel(knownTags.transparent, getAssetPath("fonts/Aller.ttf"))
 
 local stoneTex = loadTexture2d(getAssetPath("textures/Cobblestone.png"))
 local monkeyHeadMesh = sl.Mesh.loadFromFile(dev, getAssetPath("meshes/Teapot.obj"))
-createRotatingMesh(scene, effects, stoneTex, monkeyHeadMesh)
+createRotatingMesh(effects, stoneTex, monkeyHeadMesh)
 createFloor(effects, stoneTex, meshes.cube)
 
 local axesMesh = sl.Mesh.loadFromFile(dev, getAssetPath("meshes/Axes.obj"))
 local logoTex = loadTexture2d(getAssetPath("textures/Flammable.png"))
 
 attachAxesMesh(effects,
-    createMonitorQuad(scene, effects, offscreenCameraTex, meshes.quad, knownTags.monitor),
+    createMonitorQuad(effects, offscreenCameraTex, meshes.quad, knownTags.monitor),
     axesMesh)
 attachAxesMesh(effects,
-    createTransparentQuad(scene, effects, meshes.quad, knownTags.transparent, logoTex),
+    createTransparentQuad(effects, meshes.quad, knownTags.transparent, logoTex),
     axesMesh)
 
 local run = function()
