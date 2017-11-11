24621c7ea4e9b9322ddd346607fed91ab818b4a2
Aleksey Fedotov <0xc0dec@gmail.com>
2017-11-11 22:57:11 +0000

    Show axes in demo2

---
+++ assets/shaders/vulkan/Color.vert
@@ -0,0 +1,13 @@
+#version 450
+
+layout (binding = 0) uniform Matrices
+{
+    mat4 wvp;
+} matrices;
+
+layout (location = 0) in vec3 inPos;
+
+void main()
+{
+	gl_Position = matrices.wvp * vec4(inPos.xyz, 1.0);
+}
--- src/demos/demo2/Demo2.lua
+++ src/demos/demo2/Demo2.lua
@@ -108,10 +108,47 @@ function createMesh(material, position)
     end
 end
 
-material = createMaterial()
-camera = createMainCamera()
+function createAxesAttacher()
+    local mesh = sl.Mesh.loadFromFile(dev, getAssetPath("meshes/Axes.obj"))
+    local effect = sl.Effect.loadFromFiles(dev,
+        getAssetPath("shaders/vulkan/Color.vert.spv"),
+        getAssetPath("shaders/vulkan/Color.frag.spv"))
+
+    local createColorMaterial = function(color)
+        local mat = sl.Material.create(dev, effect)
+        mat:setFaceCull(sl.FaceCull.All)
+        mat:bindParameter("matrices.wvp", sl.BindParameterSemantics.WorldViewProjectionMatrix)
+        mat:setVector4Parameter("variables.color", color)
+        return mat
+    end
+
+    local materials = {
+        red = createColorMaterial(vec4(1, 0, 0, 1)),
+        green = createColorMaterial(vec4(0, 1, 0, 1)),
+        blue = createColorMaterial(vec4(0, 0, 1, 1)),
+        white = createColorMaterial(vec4(1, 1, 1, 1))
+    }
+
+    return function(node)
+        local renderer = node:addComponent("MeshRenderer")
+        renderer:setMesh(mesh)
+        renderer:setMaterial(0, materials.blue)
+        renderer:setMaterial(1, materials.green)
+        renderer:setMaterial(2, materials.white)
+        renderer:setMaterial(3, materials.red)
+    end
+end
+
+local material = createMaterial()
+local camera = createMainCamera()
 createCustomMesh(material, vec3(-2, 0, 0))
 createMesh(material, vec3(2, 0, 0))
+
+local axesNode = scene:createNode()
+local attachAxes = createAxesAttacher()
+attachAxes(axesNode)
+
+---
 
 function keepRunning()
     return not dev:isQuitRequested() and
