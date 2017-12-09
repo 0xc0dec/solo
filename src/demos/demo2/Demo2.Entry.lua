9277733b666dfd3351af30048d20b606547eb648
Aleksey Fedotov <0xc0dec@gmail.com>
2017-12-10 01:11:34 +0500

    Proper config for vulkan demo

--- src/demos/demo2/Demo2.Entry.lua
+++ src/demos/demo2/Demo2.Entry.lua
@@ -5,8 +5,16 @@
 
 deviceSetup = sl.DeviceSetup()
 deviceSetup.mode = sl.DeviceMode.Vulkan
-deviceSetup.canvasWidth = 1000
-deviceSetup.canvasHeight = 600
+deviceSetup.canvasWidth = 1200
+deviceSetup.canvasHeight = 700
+deviceSetup.fullScreen = false
+deviceSetup.windowTitle = "Demo 2"
+deviceSetup.redBits = 5
+deviceSetup.greenBits = 5
+deviceSetup.blueBits = 5
+deviceSetup.depthBits = 24
+deviceSetup.alphaBits = 0
+deviceSetup.vsync = false
 deviceSetup.logFilePath = "Demo2.log"
 
 runScript = "../../src/demos/demo2/Demo2.lua"
