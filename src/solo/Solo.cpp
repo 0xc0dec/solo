34fe00c0714b7575a8a14c4dd92f6b1bb314d22a
Aleksey Fedotov <aleksey.fedotov@gmail.com>
2018-09-30 20:41:57 +0500

    Extracted exe runner

--- src/solo/Solo.cpp
+++ src/solo/Solo.cpp
@@ -3,37 +3,8 @@
     MIT license
 */
 
-#include "SoloDevice.h"
-#include "SoloDeviceSetup.h"
-#include "SoloScriptRuntime.h"
-#include "SoloComponent.h"
+#include "Solo.h"
 
 using namespace solo;
 
 u32 ComponentTypeId::counter = 0;
-
-int main(int argc, s8 *argv[])
-{
-    if (argc <= 1)
-        return 1;
-
-    try
-    {
-        const auto entryScript = argv[1];
-        auto transientRuntime = ScriptRuntime::create();
-        transientRuntime->executeFile(entryScript);
-
-        const auto setup = transientRuntime->getDeviceSetup("deviceSetup");
-        const auto runScript = transientRuntime->getString("runScript");
-
-        const auto device = Device::create(setup);
-        device->getScriptRuntime()->executeFile(runScript);
-    }
-    catch (const std::exception &e)
-    {
-        Logger::global().logCritical(e.what());
-        return 2;
-    }
-    
-    return 0;
-}
---
+++ src/soloist/Soloist.cpp
@@ -0,0 +1,34 @@
+/*
+    Copyright (c) Aleksey Fedotov
+    MIT license
+*/
+
+#include <Solo.h>
+
+using namespace solo;
+
+int main(int argc, s8 *argv[])
+{
+    if (argc <= 1)
+        return 1;
+
+    try
+    {
+        const auto entryScript = argv[1];
+        auto transientRuntime = ScriptRuntime::create();
+        transientRuntime->executeFile(entryScript);
+
+        const auto setup = transientRuntime->getDeviceSetup("deviceSetup");
+        const auto runScript = transientRuntime->getString("runScript");
+
+        const auto device = Device::create(setup);
+        device->getScriptRuntime()->executeFile(runScript);
+    }
+    catch (const std::exception &e)
+    {
+        Logger::global().logCritical(e.what());
+        return 2;
+    }
+    
+    return 0;
+}
