ca3d11ebf446eda933b590432b58668e4eabd23b
Aleksey Fedotov <aleksey.fedotov@gmail.com>
2018-11-21 22:33:58 +0500

    Debug check in static collision mesh

--- src/solo/bullet/SoloBulletStaticMeshCollider.cpp
+++ src/solo/bullet/SoloBulletStaticMeshCollider.cpp
@@ -12,13 +12,14 @@ using namespace solo;
 BulletStaticMeshCollider::BulletStaticMeshCollider(sptr<MeshData> data):
     data_(data)
 {
-    // TODO more versatile?
+    SL_DEBUG_PANIC(data_->indexData().empty(), "Collision mesh index data is empty");
+
     mesh_ = std::make_unique<btIndexedMesh>();
     mesh_->m_indexType = PHY_INTEGER;
     mesh_->m_vertexType = PHY_FLOAT;
-    mesh_->m_numTriangles = data_->indexData()[0].size() / 3;
+    mesh_->m_numTriangles = data_->indexData().front().size() / 3;
     mesh_->m_numVertices = data_->vertexCount();
-    mesh_->m_triangleIndexBase = reinterpret_cast<const u8*>(data_->indexData()[0].data());
+    mesh_->m_triangleIndexBase = reinterpret_cast<const u8*>(data_->indexData().front().data());
     mesh_->m_triangleIndexStride = 3 * sizeof(u32);
     mesh_->m_vertexBase = reinterpret_cast<const u8*>(data_->vertexData().data());
     mesh_->m_vertexStride = 3 * sizeof(float);
