4c3ec49e6f464582296d1fff4f93a5a1dffa2ab4
Aleksey Fedotov <aleksey.fedotov@gmail.com>
2018-11-22 22:02:05 +0500

    Extracted light camera func

--- src/demos/lua/demo1/demo1.lua
+++ src/demos/lua/demo1/demo1.lua
@@ -27,39 +27,9 @@ function demo()
     local attachAxes = (require "axes")(assetCache)
     local createLookAt = require "look-at"
     local createHighlighter = require "highlighter"
+    local createLightCamera = require "light-camera"
 
     ---
-
-    function createLightCamera()
-        local node = scene:createNode()
-
-        local cam = node:addComponent("Camera")
-        cam:setZNear(0.05)
-        cam:setZFar(1000)
-        cam:setFieldOfView(sl.Radians.fromRawDegrees(50))
-        cam:setViewport(vec4(0, 0, 2048, 2048))
-
-        local depthTex = sl.Texture2D.empty(sl.device, 2048, 2048, sl.TextureFormat.Depth24)
-        depthTex:setFilter(sl.TextureFilter.Nearest, sl.TextureFilter.Nearest, sl.TextureMipFilter.Nearest)
-
-        local fb = sl.FrameBuffer.create(sl.device, { depthTex })
-        cam:setRenderTarget(fb)
-    
-        local transform = node:findComponent("Transform")
-        transform:setLocalPosition(vec3(-20, 20, -20))
-        transform:lookAt(vec3(0, 0, 0), vec3(0, 1, 0))
-
-        local rootNode = scene:createNode()
-        rootNode:addScriptComponent(createRotator("world", vec3(0, 1, 0), 0.3))
-        transform:setParent(rootNode:findComponent("Transform"))
-
-        return {
-            camera = cam,
-            node = node,
-            transform = transform,
-            depthTex = depthTex
-        }
-    end
 
     function createMesh(meshPath, material)
         local node = scene:createNode()
@@ -125,7 +95,7 @@ function demo()
 
     ---
 
-    local lightCam = createLightCamera()
+    local lightCam = createLightCamera(scene)
 
     local colorPassEffect = assetCache.getEffect("shadowed")
     local colorPassMaterial = sl.Material.fromEffect(sl.device, colorPassEffect)
---
+++ src/demos/lua/demo1/light-camera.lua
@@ -0,0 +1,37 @@
+--
+-- Copyright (c) Aleksey Fedotov
+-- MIT license
+-- 
+
+local createRotator = require "rotator"
+
+return function(scene)
+    local node = scene:createNode()
+
+    local cam = node:addComponent("Camera")
+    cam:setZNear(0.05)
+    cam:setZFar(1000)
+    cam:setFieldOfView(sl.Radians.fromRawDegrees(50))
+    cam:setViewport(vec4(0, 0, 2048, 2048))
+
+    local depthTex = sl.Texture2D.empty(sl.device, 2048, 2048, sl.TextureFormat.Depth24)
+    depthTex:setFilter(sl.TextureFilter.Nearest, sl.TextureFilter.Nearest, sl.TextureMipFilter.Nearest)
+
+    local fb = sl.FrameBuffer.create(sl.device, { depthTex })
+    cam:setRenderTarget(fb)
+
+    local transform = node:findComponent("Transform")
+    transform:setLocalPosition(vec3(-20, 20, -20))
+    transform:lookAt(vec3(0, 0, 0), vec3(0, 1, 0))
+
+    local rootNode = scene:createNode()
+    rootNode:addScriptComponent(createRotator("world", vec3(0, 1, 0), 0.3))
+    transform:setParent(rootNode:findComponent("Transform"))
+
+    return {
+        camera = cam,
+        node = node,
+        transform = transform,
+        depthTex = depthTex
+    }
+end
