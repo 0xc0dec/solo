582f590f0010a065ebe0cea796d43f3b1728e5c7
Aleksey Fedotov <aleksey.fedotov@gmail.com>
2020-05-27 23:10:40 +0500

    Tests for node

---
+++ src/lua-tests/node.lua
@@ -0,0 +1,27 @@
+local scene = sl.Scene.empty(sl.device)
+local node = scene:createNode()
+
+assert(node:id() ~= nil)
+assert(node:scene() == scene)
+
+local t = node:findComponent('Transform')
+assert(t)
+
+local cam = node:addComponent('Camera')
+node:removeComponent('Camera')
+
+assert(node:findComponent('Camera') == nil)
+
+local cmp = node:addScriptComponent(sl.createComponent("TestyTest", {
+    abc = 123,
+
+    update = function(self)
+    end
+}))
+
+cmp = node:findScriptComponent(sl.cmpNameToId("TestyTest"))
+-- findScriptComponent will always return a different wrapper, so direct comparison doesn't work
+assert(cmp.abc == 123)
+
+node:removeScriptComponent(cmp)
+assert(node:findScriptComponent(sl.cmpNameToId("TestyTest")) == nil)
--- src/lua-tests/tests.lua
+++ src/lua-tests/tests.lua
@@ -29,6 +29,7 @@ function run()
     runTest('effect')
     runTest('file-system')
     runTest('renderer')
+    runTest('node')
 end
 
 callSafe(run)
