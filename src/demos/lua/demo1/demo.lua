94cf87737cf68d4510a8c40983b9160be2de1a66
Aleksey Fedotov <aleksey.fedotov@gmail.com>
2020-06-07 16:21:41 +0500

    Demo1: extracted static mesh creation into a module; added new house mesh

--- src/demos/lua/demo1/demo.lua
+++ src/demos/lua/demo1/demo.lua
@@ -3,10 +3,10 @@
 -- MIT license
 -- 
 
-sl.includeDir("../../../src/demos/lua/demo1")
-sl.includeDir("../../../src/demos/lua/common")
+sl.includeDir('../../../src/demos/lua/demo1')
+sl.includeDir('../../../src/demos/lua/common')
 
-require "common"
+require 'common'
 
 function demo()
     local fs = sl.device:fileSystem()
@@ -15,112 +15,68 @@ function demo()
     local scene = sl.Scene.empty(sl.device)
     local ui = sl.device:debugInterface()
 
-    local tags = require "tags"
-    local createRotator = require "rotator"
-    local createMainCamera = require "main-camera"
-    local createSkybox = require "skybox"
-    local assetCache = (require "asset-cache")()
-    local createPostProcessor = require "post-processor"
-    local createPostProcessorControlPanel = require "post-processor-control-panel"
-    local createTracer = require "tracer"
-    local createSpawner = require "spawner"
-    local createCheckerBox = require "checker-box"
-    local createDynamicQuad = require "dynamic-quad"
-    local attachAxes = (require "axes")(assetCache)
-    local createLookAt = require "look-at"
-    local createHighlighter = require "highlighter"
-    local createLightCamera = require "light-camera"
-    local createGrabber = require "grabber"
+    local tags = require 'tags'
+    local createRotator = require 'rotator'
+    local createMainCamera = require 'main-camera'
+    local createSkybox = require 'skybox'
+    local assetCache = (require 'asset-cache')()
+    local createPostProcessor = require 'post-processor'
+    local createPostProcessorControlPanel = require 'post-processor-control-panel'
+    local createTracer = require 'tracer'
+    local createSpawner = require 'spawner'
+    local createCheckerBox = require 'checker-box'
+    local createDynamicQuad = require 'dynamic-quad'
+    local attachAxes = (require 'axes')(assetCache)
+    local createLookAt = require 'look-at'
+    local createHighlighter = require 'highlighter'
+    local createLightCamera = require 'light-camera'
+    local createGrabber = require 'grabber'
+    local createStaticMesh = (require 'static-mesh')(scene)
 
     ---
 
-    function createMesh(meshPath, material, onMeshLoad)
-        local node = scene:createNode()
-    
-        local renderer = node:addComponent("MeshRenderer")
-        renderer:setDefaultMaterial(material)
-    
-        local transform = node:findComponent("Transform")
-    
-        local layout = sl.VertexBufferLayout()
-        layout:addAttribute(sl.VertexAttributeUsage.Position)
-        layout:addAttribute(sl.VertexAttributeUsage.Normal)
-        layout:addAttribute(sl.VertexAttributeUsage.Tangent)
-        layout:addAttribute(sl.VertexAttributeUsage.TexCoord)
-        sl.Mesh.fromFileAsync(sl.device, assetPath(meshPath), layout)
-            :done(function(mesh)
-                renderer:setMesh(mesh)
-                onMeshLoad(mesh)
-            end)
-
-        return {
-            node = node,
-            transform = transform,
-            renderer = renderer
-        }
-    end
-
     function createTracker(parentTransform, pos, target)
         local node = scene:createNode()
-        local transform = node:findComponent("Transform")
+        local transform = node:findComponent('Transform')
         transform:setParent(parentTransform)
         transform:setLocalPosition(pos)
         node:addScriptComponent(createLookAt(target))
         attachAxes(node)
     end
 
-    function createStaticColliderMesh(meshPath, material)
-        local body
-
-        local context = createMesh(meshPath, material,
-            function(mesh)
-                local col = sl.StaticMeshCollider.fromMesh(mesh)
-                body:setCollider(col)
-            end)
-        
-        local params = sl.RigidBodyParams()
-        params.mass = 0
-        params.friction = 0.5
-        
-        body = context.node:addComponent("RigidBody", params)
-        body:setStatic(true)
-        
-        return context
-    end
-
     function createBackdrop(material)
-        return createStaticColliderMesh("meshes/backdrop.obj", material)
+        return createStaticMesh('meshes/backdrop.obj', material)
     end
 
     function createHouse(material)
-        return createStaticColliderMesh('meshes/house.obj', material)
+        return createStaticMesh('meshes/house2.obj', material)
     end
 
     function createTeapot(material)
-        local context = createStaticColliderMesh("meshes/teapot.obj", material)
-        context.transform:setLocalPosition(vec3(3, 0, -3))
+        local result = createStaticMesh('meshes/teapot.obj', material)
+        result.transform:setLocalPosition(vec3(3, 0, -3))
     end
 
     function createShadowedMaterial(lightCam)
-        local eff = assetCache.effect("shadowed")
+        local eff = assetCache.effect('shadowed')
         local mat = sl.Material.fromEffect(sl.device, eff)
         mat:setFaceCull(sl.FaceCull.None)
-        mat:bindParameter("uniforms:wvp", sl.ParameterBinding.WorldViewProjectionMatrix)
-        mat:bindParameter("uniforms:world", sl.ParameterBinding.WorldMatrix)
-        mat:bindParameter("uniforms:camPos", sl.ParameterBinding.CameraWorldPosition)
-        mat:setTextureParameter("colorMap", assetCache.textures.texture1.color)
-        mat:setTextureParameter("normalMap", assetCache.textures.texture1.normal)
-        mat:setTextureParameter("shadowMap", lightCam.depthTex)
-        mat:bindMatrixParameter("uniforms:lightVp", function() return lightCam.camera:viewProjectionMatrix() end)
-        mat:bindVector3Parameter("uniforms:lightDir", function() return lightCam.transform:worldForward() end)
+        mat:bindParameter('uniforms:wvp', sl.ParameterBinding.WorldViewProjectionMatrix)
+        mat:bindParameter('uniforms:world', sl.ParameterBinding.WorldMatrix)
+        mat:bindParameter('uniforms:camPos', sl.ParameterBinding.CameraWorldPosition)
+        mat:setTextureParameter('colorMap', assetCache.textures.texture1.color)
+        mat:setTextureParameter('normalMap', assetCache.textures.texture1.normal)
+        mat:setTextureParameter('shadowMap', lightCam.depthTex)
+        mat:bindMatrixParameter('uniforms:lightVp', function() return lightCam.camera:viewProjectionMatrix() end)
+        mat:bindVector3Parameter('uniforms:lightDir', function() return lightCam.transform:worldForward() end)
         mat:setFloatParameter('variables:highlighted', 0)
         return mat
     end
 
     function createSpectatorCamera(shadowedMat)
         local camera, node = createMainCamera(scene)
-        node:findComponent("Transform"):setLocalPosition(vec3(10, 10, -5))
-        node:findComponent("Transform"):lookAt(vec3(0, 2, 0), vec3(0, 1, 0))
+        node:findComponent('Transform'):setLocalPosition(vec3(10, 10, -5))
+        node:findComponent('Transform'):lookAt(vec3(0, 2, 0), vec3(0, 1, 0))
         node:addScriptComponent(createTracer(sl.device, scene, physics, assetCache))
         node:addScriptComponent(createHighlighter(assetCache, physics))
         node:addScriptComponent(createSpawner(assetCache, shadowedMat))
@@ -134,9 +90,9 @@ function demo()
 
     function createAxes()
         local rootNode = scene:createNode()
-        local rootTransform = rootNode:findComponent("Transform")
+        local rootTransform = rootNode:findComponent('Transform')
         attachAxes(rootNode)
-        rootNode:addScriptComponent(createRotator("world", vec3(0, 1, 0), 1))
+        rootNode:addScriptComponent(createRotator('world', vec3(0, 1, 0), 1))
 
         createTracker(rootTransform, vec3(4, 4, 4), vec3(3, 0, -3))
         createTracker(rootTransform, vec3(-4, 4, 4), vec3(-3, 1, 3))
@@ -221,7 +177,7 @@ Controls:
         scene:update()
         lightCam.camera:renderFrame(renderLightCamFrame)
         mainCamera.camera:renderFrame(renderMainCamFrame)
-        -- postProcessor:apply()
+        postProcessor:apply()
         renderUi()
     end
 
---
+++ src/demos/lua/demo1/static-mesh.lua
@@ -0,0 +1,41 @@
+--
+-- Copyright (c) Aleksey Fedotov
+-- MIT license
+-- 
+
+require 'common'
+
+return function(scene)
+    return function(path, material)
+        local node = scene:createNode()
+        
+        local body
+
+        local renderer = node:addComponent('MeshRenderer')
+        renderer:setDefaultMaterial(material)
+
+        local layout = sl.VertexBufferLayout()
+        layout:addAttribute(sl.VertexAttributeUsage.Position)
+        layout:addAttribute(sl.VertexAttributeUsage.Normal)
+        layout:addAttribute(sl.VertexAttributeUsage.Tangent)
+        layout:addAttribute(sl.VertexAttributeUsage.TexCoord)
+        sl.Mesh.fromFileAsync(sl.device, assetPath(path), layout)
+            :done(function(mesh)
+                renderer:setMesh(mesh)
+                body:setCollider(sl.StaticMeshCollider.fromMesh(mesh))
+            end)
+
+        local bodyParams = sl.RigidBodyParams()
+        bodyParams.mass = 0
+        bodyParams.friction = 0.5
+
+        body = node:addComponent('RigidBody', bodyParams)
+        body:setStatic(true)
+
+        return {
+            node = node,
+            transform = node:findComponent('Transform'),
+            renderer = renderer
+        }
+    end
+end
