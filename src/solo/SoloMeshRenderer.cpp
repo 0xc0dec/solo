112af0cbde2361f916366dec8edc280063182e61
Aleksey Fedotov <0xc0dec@gmail.com>
2023-02-26 16:35:08 +0500

    Render error mesh if the real mesh is missing

--- src/solo/SoloMeshRenderer.cpp
+++ src/solo/SoloMeshRenderer.cpp
@@ -11,10 +11,52 @@
 
 using namespace solo;
 
+sptr<Mesh> MeshRenderer::errorMesh_ = nullptr;
+
 MeshRenderer::MeshRenderer(const Node &node):
     ComponentBase(node),
     renderer_(node.scene()->device()->renderer()) {
     transform_ = node.findComponent<Transform>();
+
+    if (!errorMesh_) {
+        errorMesh_ = Mesh::empty(node.scene()->device());
+        auto layout = VertexBufferLayout();
+        layout.addAttribute(VertexAttributeUsage::Position);
+
+        errorMesh_->addVertexBuffer(layout, {
+            // front
+            -1.0, -1.0,  1.0,
+             1.0, -1.0,  1.0,
+             1.0,  1.0,  1.0,
+            -1.0,  1.0,  1.0,
+            // back
+            -1.0, -1.0, -1.0,
+             1.0, -1.0, -1.0,
+             1.0,  1.0, -1.0,
+            -1.0,  1.0, -1.0
+        }, 8);
+        errorMesh_->addIndexBuffer({
+            // front
+		    0, 1, 2,
+		    2, 3, 0,
+		    // right
+		    1, 5, 6,
+		    6, 2, 1,
+		    // back
+		    7, 6, 5,
+		    5, 4, 7,
+		    // left
+		    4, 0, 3,
+		    3, 7, 4,
+		    // bottom
+		    4, 5, 1,
+		    1, 0, 4,
+		    // top
+		    3, 2, 6,
+		    6, 7, 3
+        }, 36);
+        errorMesh_->setPrimitiveType(PrimitiveType::Triangles);
+    }
 }
 
 auto MeshRenderer::material(u32 index) const -> sptr<Material> {
@@ -23,17 +65,16 @@ auto MeshRenderer::material(u32 index) const -> sptr<Material> {
 }
 
 void MeshRenderer::render() {
-    if (!mesh_)
-        return;
+    const auto mesh = mesh_ ? mesh_ : errorMesh_;
 
-    const auto indexCount = mesh_->indexBufferCount();
+    const auto indexCount = mesh->indexBufferCount();
     if (!indexCount) {
         const auto mat = material(0);
-        renderer_->renderMesh(mesh_.get(), transform_, mat.get());
+        renderer_->renderMesh(mesh.get(), transform_, mat.get());
     } else {
         for (u32 index = 0; index < indexCount; ++index) {
             const auto mat = material(index);
-            renderer_->renderMeshIndex(mesh_.get(), index, transform_, mat.get());
+            renderer_->renderMeshIndex(mesh.get(), index, transform_, mat.get());
         }
     }
 }
--- src/solo/SoloMeshRenderer.h
+++ src/solo/SoloMeshRenderer.h
@@ -22,18 +22,15 @@ namespace solo {
         void render() override;
 
         auto mesh() const -> sptr<Mesh> { return mesh_; }
-        void setMesh(const sptr<Mesh> &mesh) {
-            this->mesh_ = mesh;
-        }
+        void setMesh(const sptr<Mesh> &mesh) { this->mesh_ = mesh; }
 
         auto material(u32 index) const -> sptr<Material>;
-        auto materialCount() const -> u32 {
-            return materialCount_;
-        }
+        auto materialCount() const -> u32 { return materialCount_; }
         void setMaterial(u32 index, const sptr<Material> &material);
         void setDefaultMaterial(const sptr<Material> &material);
 
     private:
+        static sptr<Mesh> errorMesh_;
         sptr<Mesh> mesh_;
         Transform *transform_ = nullptr;
         Renderer *renderer_ = nullptr;
